fdfdf9ac3cd6b3d8fc0c25f8b7152cdce2271411
Merlin Opp <ufjqw@student.kit.edu>
2022-06-18 09:43:04 +0200

    Start implementation of DatasetCleaning.py

--- backend_library/src/main/backend/task/cleaning/DatasetCleaning.py
+++ backend_library/src/main/backend/task/cleaning/DatasetCleaning.py
@@ -12,10 +12,10 @@ from collections.abc import Callable
 class DatasetCleaning(Task):
     original_dataset_path: string
     cleaned_dataset_path: string
-    cleaning_steps: List[DatasetCleaningStep] = []
+    cleaning_steps = []  # DatasetCleaningStep[]
 
     def __init__(self, user_id: int, task_id: int, task_progress_callback: Callable, original_dataset_path: string,
-                 cleaned_dataset_path: string, cleaning_steps: List[DatasetCleaningStep]):
+                 cleaned_dataset_path: string, cleaning_steps):
         Task.__init__(user_id, task_id, task_progress_callback)
         self.original_dataset_path = original_dataset_path
         self.cleaned_dataset_path = cleaned_dataset_path
@@ -39,19 +39,23 @@ class DatasetCleaning(Task):
         return None
 
     def run_cleaning_pipeline(self, csv_to_clean) -> None:
-        # TODO Implement
-        # TODO Error Handling!! (Vorallem für Callback)
-
-        finished_cleaning_steps: 0
+        finished_cleaning_steps: int = 0
 
         for cleaning_step in self.cleaning_steps:
-            cleaning_step.do_cleaning()
+            try:
+                cleaning_step.do_cleaning(csv_to_clean)
+            except:  # catch *all* exceptions
+                # TODO: Rename CSV file -> .error
+                self.progress_update_callback(self.task_id, TaskState.FINISHED_WITH_ERROR, 1)
+                return None
+
             finished_cleaning_steps += 1
             progress: float = finished_cleaning_steps / len(self.cleaning_steps)
             self.progress_update_callback(self.task_id, TaskState.RUNNING, progress)
-        self.progress_update_callback(self.task_id, TaskState.FINISHED, 1)
 
-        return None
+        self.progress_update_callback(self.task_id, TaskState.FINISHED, 1)
+        # TODO: Implement Test
+
 
     def store_cleaned_dataset(self, cleaned_dataset) -> None:
         # TODO Implement
