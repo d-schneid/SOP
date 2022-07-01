b317440df52b85771ac389af09135460a7e624e3
Merlin Opp <ufjqw@student.kit.edu>
2022-07-01 09:03:48 +0200

    Implement my part of ExecutionElement

--- backend_library/src/main/backend/task/execution/core/ExecutionElement.py
+++ backend_library/src/main/backend/task/execution/core/ExecutionElement.py
@@ -1,4 +1,6 @@
+import os
 import string
+from abc import ABC
 
 from ExecutionSubspace import ExecutionSubspace
 import numpy as np
@@ -7,7 +9,7 @@ from backend_library.src.main.backend.task.execution.ParameterizedAlgorithm impo
 from backend_library.src.main.backend.scheduler.Schedulable import Schedulable
 
 
-class ExecutionElement(Schedulable):
+class ExecutionElement(Schedulable, ABC):
     def __init__(self, execution_subspace: ExecutionSubspace, algorithm: ParameterizedAlgorithm, result_path: string):
         self._execution_subspace: ExecutionSubspace = execution_subspace
         self._algorithm: ParameterizedAlgorithm = algorithm
@@ -17,22 +19,29 @@ class ExecutionElement(Schedulable):
         self._task_id = self._execution_subspace.task_id
 
     def finished_result_exists(self) -> bool:
-        # TODO
-        pass
+        return os.path.isfile(self._result_path)
+
+    # Schedulable
+    def user_id(self) -> int:
+        return self._user_id
+
+    def task_id(self) -> int:
+        return self._task_id
 
     def do_work(self) -> None:
         run_algo_result: np.ndarray = self.__run_algorithm()
         result_to_save: np.ndarray = self.__convert_result_to_csv(run_algo_result)
         self.__save_result(self._result_path, result_to_save)
 
+    # do_work()
     def __run_algorithm(self) -> np.ndarray:
-        # TODO
-        pass
+        # TODO: Tobias
+        return np.zeros(0)
 
     def __convert_result_to_csv(self, run_algo_result: np.ndarray) -> np.ndarray:
-        # TODO
-        pass
+        # TODO: Tobias
+        return np.zeros(0)
 
     def __save_result(self, path: string, result: np.ndarray) -> None:
-        # TODO
+        # TODO: Tobias
         pass
