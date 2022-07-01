d75857dc1795154fa9c993aeb58a6d4a9da602bd
Merlin Opp <ufjqw@student.kit.edu>
2022-07-01 09:18:22 +0200

    Clean SubspaceGenerationDescription

--- backend_library/src/main/backend/task/execution/subspace/SubspaceGenerationDescription.py
+++ backend_library/src/main/backend/task/execution/subspace/SubspaceGenerationDescription.py
@@ -6,16 +6,12 @@ from backend_library.src.main.backend.task.execution.subspace.Subspace import Su
 
 
 class SubspaceGenerationDescription(ABC):
-    @abstractmethod
-    def parameters_are_correct(self) -> bool:
-        return True
-
     # returns a Subspace[]
     @abstractmethod
     def generate(self) -> Iterable[Subspace]:
-        return Subspace[0]
+        return iter([])
 
+    # Create a dictionary first with all values that should be visualized and convert it into the json
     @abstractmethod
     def to_json(self) -> string:
-        # Create a dictionary first with all values that should be visualized and convert it into the json
         return ""
