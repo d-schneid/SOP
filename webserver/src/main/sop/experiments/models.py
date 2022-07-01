2226ff9c15295a3c5593ffd640ff9862050faae6
Jonas Bergmann <uhwkd@student.kit.edu>
2022-07-01 20:59:57 +0200

    Implement ExperimentModel

--- webserver/src/main/sop/experiments/models.py
+++ webserver/src/main/sop/experiments/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import CharField
 
 
 class DatasetModel(models.Model):
@@ -10,7 +11,48 @@ class AlgorithmModel(models.Model):
 
 
 class ExperimentModel(models.Model):
-    pass
+    """
+    Database model of an experiment
+    """
+    _display_name = models.CharField(max_length=80)
+    _user = models.ForeignKey(to="authentication.User",
+                              on_delete=models.CASCADE)
+    # We do not allow deletion of a dataset if it's used in an experiment
+    _dataset = models.ForeignKey(to=DatasetModel, on_delete=models.PROTECT,
+                                 related_name="experiment")
+    _algorithms = models.ManyToManyField(to=AlgorithmModel)
+    _creation_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ["_display_name", "_user"]
+
+    @property
+    def display_name(self) -> models.CharField:
+        return self._display_name
+
+    @display_name.setter
+    def display_name(self, value):
+        # TODO: maybe throw exception if value has more than 80 characters?
+        self._display_name = value
+
+    @property
+    def user(self) -> models.ForeignKey:
+        return self._user
+
+    @property
+    def dataset(self) -> models.ForeignKey:
+        return self._dataset
+
+    @property
+    def algorithms(self) -> models.ManyToManyField:
+        return self._algorithms
+
+    @property
+    def creation_date(self) -> models.DateTimeField:
+        return self._creation_date
+
+    def __str__(self) -> str:
+        return str(self.display_name)
 
 
 class ExecutionModel(models.Model):
