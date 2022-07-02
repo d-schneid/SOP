2ce8eba6ec1e9dfeb671fe2c8e3930784cebcaf1
Tobias <tobrohl@gmail.com>
2022-07-02 10:36:39 +0200

    Add Subspace class header

--- backend_library/src/main/backend/task/execution/subspace/Subspace.py
+++ backend_library/src/main/backend/task/execution/subspace/Subspace.py
@@ -1,2 +1,23 @@
+import numpy as np
+
+
 class Subspace:
-    pass
+    def __init__(self, mask: np.array):
+        self._mask = mask
+
+    @property
+    def mask(self):
+        return self._mask
+
+    def get_included_dimension_count(self) -> int:
+        pass
+
+    def get_subspace_identifier(self) -> str:
+        pass
+
+    def get_size_of_subspace_buffer(self, full_dataset: np.array) -> int:
+        pass
+
+    def make_subspace_array(self, full_dataset: np.array, target_buffer : memoryview) -> np.array:
+        pass
+
