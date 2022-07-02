9795ff8bbb46794b75d225d16228fe09f76dca98
Daniel Schneider <daniel.schneider@student.kit.edu>
2022-07-02 16:32:40 +0200

    Add custom UserAdmin

--- webserver/src/main/sop/authentication/admin.py
+++ webserver/src/main/sop/authentication/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth.models import Group
+from django.utils.translation import gettext as _
 
-# Register your models here.
+from .models import User
+
+# Not needed, an admin (superuser) of the system shall have all possible permissions
+admin.site.unregister(Group)
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    """
+    Custom User model that is displayed in the admin panel.
+    """
+
+    model = User
+
+    list_filter = ("is_superuser", "is_active")
+    list_display = ("username", "is_superuser", "is_active")
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    # blocks a User
+                    "is_active",
+
+                    # assigns admin status to a User
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
