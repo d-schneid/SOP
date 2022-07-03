2225997e7b0dea0c0f3711d4315c5277588c0ff0
Merlin Opp <ufjqw@student.kit.edu>
2022-07-03 11:25:28 +0200

    Document ImputationMode, ThresholdMissingValuesRemover, MinMaxScaler and CategoricalColumnRemover.

--- backend_library/src/main/backend/task/cleaning/CategoricalColumnRemover.py
+++ backend_library/src/main/backend/task/cleaning/CategoricalColumnRemover.py
@@ -7,9 +7,15 @@ from backend_library.src.main.backend.task.cleaning.CategoricalDataHandler impor
 
 
 class CategoricalColumnRemover(CategoricalDataHandler, ABC):
-    def __init__(self):
-        pass
+    """
+    A cleaning_step for the dataset cleaning that removes all columns that contain categorical data.
+    """
 
     def do_cleaning(self, dataset_to_clean: np.ndarray) -> np.ndarray:
+        """
+        Remove all columns that contain categorical data. \n
+        :param dataset_to_clean: The dataset that will be cleaned in this cleaning_step.
+        :return: The cleaned dataset.
+        """
         df = pd.DataFrame(dataset_to_clean)
         return df.select_dtypes(exclude=['char']).to_numpy()
--- backend_library/src/main/backend/task/cleaning/ThresholdMissingValuesRemover.py
+++ backend_library/src/main/backend/task/cleaning/ThresholdMissingValuesRemover.py
@@ -8,14 +8,28 @@ from MissingValuesRemover import MissingValuesRemover
 
 
 class ThresholdMissingValuesRemover(MissingValuesRemover, ABC):
+    """
+    A cleaning_step for the dataset cleaning that rows that have to many missing values.
+    """
     # threshold is a value between 0 and 1. It shows the proportion of
     # minimum None entries that a column that will be removed should have
     # Note: If threshold = 1 -> All data points are removed
     # Note: If threshold = 0 -> Only data points are removed, that have only missing values
     def __init__(self, threshold: float = 1):
+        """
+        :param threshold: Is a value in [0,1]. It shows the minimum percentual number/count of missing values that a row
+        that will be removed will have. \n
+            Note: If threshold = 1 -> All data points are removed \n
+            Note: If threshold = 0 -> Only data points are removed, that have only missing values \n
+        """
         self._threshold: float = max(0.0, min(1.0, threshold))  # clamp threshold to [0,1]
 
     def do_cleaning(self, dataset_to_clean: np.ndarray) -> np.ndarray:
+        """
+        Removes rows that have more missing values than the threshold allows. \n
+        :param dataset_to_clean: The dataset that will be cleaned in this cleaning_step.
+        :return: The cleaned dataset.
+        """
         row_count: int = dataset_to_clean.shape[0]  # TODO: Check if 0 or 1 in array
         absolute_threshold: int = max(1, math.ceil(self._threshold * row_count))
         df = pd.DataFrame(dataset_to_clean)
