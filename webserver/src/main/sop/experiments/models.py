9d1e3e8c049af404c4a50e4dc6d11c59b22ba3ae
Daniel Schneider <daniel.schneider@student.kit.edu>
2022-07-04 03:01:49 +0200

    Implement AlgorithmModel

--- webserver/src/main/sop/experiments/models.py
+++ webserver/src/main/sop/experiments/models.py
@@ -1,13 +1,77 @@
 from django.db import models
-from django.db.models import CharField
+from django.conf import settings
+from django.core.validators import FileExtensionValidator
+
+from .validators import validate_file_extension
+from .managers import AlgorithmManager
 
 
 class DatasetModel(models.Model):
     pass
 
+def get_algorithm_upload_path(instance, filename) -> str:
+    return f"algorithms/user_{instance.user.id}/{filename}"
 
 class AlgorithmModel(models.Model):
-    pass
+    """
+    Database model of an algorithm
+    """
+
+    # TODO: more groups to be added
+    class AlgorithmGroup(models.TextChoices):
+        LINEAR_MODEL = "Linear Model"
+        PROXIMITY_BASED = "Proximity-based"
+        PROBABILISTIC = "Probabilistic"
+        OUTLIER_ENSEMBLES = "Outlier Ensembles"
+        OTHER = "Other"
+
+    # TODO: check max_length, blank, and null
+    _name = models.CharField(max_length=80)
+    _group = models.CharField(max_length=80, choices=AlgorithmGroup.choices)
+    _signature = models.CharField(max_length=80)
+    _path = models.FileField(upload_to=get_algorithm_upload_path,
+                             validators=(validate_file_extension,
+                                         FileExtensionValidator(allowed_extensions=[".py"]),))
+    _description = models.TextField()
+    _user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    objects = AlgorithmManager()
+
+    class Meta:
+        # TODO: use UniqueConstraint instead
+        unique_together =["_name", "_user"]
+
+    @property
+    def name(self) -> models.CharField:
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        # TODO: maybe throw exception if value has more than 80 characters?
+        self._name = value
+
+    @property
+    def group(self) -> models.CharField:
+        return self._group
+
+    @property
+    def signature(self) -> models.CharField:
+        return self._signature
+
+    @property
+    def path(self) -> models.FileField:
+        return self._path
+
+    @property
+    def description(self) -> models.TextField:
+        return self._description
+
+    @property
+    def user(self) -> models.ForeignKey:
+        return self._user
+
+    def __str__(self) -> str:
+        return str(self.name) + "|" + str(self.group)
 
 
 class ExperimentModel(models.Model):
@@ -15,7 +79,7 @@ class ExperimentModel(models.Model):
     Database model of an experiment
     """
     _display_name = models.CharField(max_length=80)
-    _user = models.ForeignKey(to="authentication.User",
+    _user = models.ForeignKey(to=settings.AUTH_USER_MODEL,
                               on_delete=models.CASCADE)
     # We do not allow deletion of a dataset if it's used in an experiment
     _dataset = models.ForeignKey(to=DatasetModel, on_delete=models.PROTECT,
