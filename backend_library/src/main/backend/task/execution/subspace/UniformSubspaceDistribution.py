b656780d3ec50f7f6031dcb238ab858a2757781e
Tobias <tobrohl@gmail.com>
2022-07-04 21:52:47 +0200

    Implement SubspaceSizeDistribution

---
+++ backend_library/src/main/backend/task/execution/subspace/UniformSubspaceDistribution.py
@@ -0,0 +1,6 @@
+from backend_library.src.main.backend.task.execution.subspace.SubspaceSizeDistribution import SubspaceSizeDistribution
+
+
+class UniformSubspaceDistribution(SubspaceSizeDistribution):
+    def to_json(self) -> str:
+        pass
