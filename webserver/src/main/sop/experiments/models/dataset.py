72b33f945ed00faea3e603e641219131bdb48b7b
Felix Zimmer <felix.zimmer@student.kit.edu>
2022-07-05 02:36:48 +0200

    Add Properties for Dataset Model.

--- webserver/src/main/sop/experiments/models/dataset.py
+++ webserver/src/main/sop/experiments/models/dataset.py
@@ -1,10 +1,16 @@
 from django.conf import settings
 from django.db import models
 
+from authentication.models import User
+from experiments.models.managers import DatasetManager
+
 
 class Dataset(models.Model):
+    """
+    Database Model of a Dataset.
+    """
     _name = models.CharField(max_length=80)
-    _description = models.TextField()
+    _description = models.TextField(max_length=255)
     _user = models.ForeignKey(to=settings.AUTH_USER_MODEL,
                               on_delete=models.CASCADE)
     _datapoints_total = models.IntegerField()
@@ -12,5 +18,50 @@ class Dataset(models.Model):
     _path_original = models.FileField()
     _path_cleaned = models.FilePathField()
     _is_cleaned = models.BooleanField()
-    # TODO
-    objects = ...
+    _objects = DatasetManager()
+
+    class Meta:
+        unique_together = ["_name", "_user"]
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def description(self) -> str:
+        return self._description
+
+    @property
+    def user(self) -> User:
+        return self._user
+
+    @property
+    def datapoints_total(self) -> int:
+        return self._datapoints_total
+
+    @property
+    def dimensions_total(self) -> int:
+        return self._dimensions_total
+
+    @property
+    def path_original(self) -> str:
+        return self._path_original.path
+
+    @property
+    def path_cleaned(self) -> str:
+        return self._path_cleaned
+
+    @property
+    def is_cleaned(self) -> bool:
+        return self._is_cleaned
+
+    @property
+    def objects(self) -> DatasetManager:
+        return self._objects
+
+    def __str__(self) -> str:
+        return str(self._name) + "|" + str(self._user)
+
+
+def get_dataset_path(instance: Dataset, filename: str) -> str:
+    return f"datasets/user_{instance.user.pk}/{filename}"
