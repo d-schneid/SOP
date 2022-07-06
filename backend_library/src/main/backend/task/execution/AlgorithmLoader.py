c40afa7c15588c483580afd2f4f1a3ca65e9569d
Tobias <tobrohl@gmail.com>
2022-07-06 16:09:19 +0200

    implement AlgorithmLoader bodies (except validation)

--- backend_library/src/main/backend/task/execution/AlgorithmLoader.py
+++ backend_library/src/main/backend/task/execution/AlgorithmLoader.py
@@ -1,5 +1,10 @@
+import importlib
 import inspect
-from typing import Dict, Type
+import os.path
+import pathlib
+import sys
+from types import ModuleType
+from typing import Dict, Type, Optional
 
 from pyod.models.base import BaseDetector
 
@@ -9,24 +14,38 @@ class AlgorithmLoader:
 
     @staticmethod
     def set_algorithm_root_dir(directory: str):
+        if directory[-1] in ['/', '\\']:
+            directory=directory[:-1]
         AlgorithmLoader._root_dir = directory
 
     @staticmethod
     def ensure_root_dir_in_path():
+        if AlgorithmLoader._root_dir not in sys.path:
+            sys.path.append(AlgorithmLoader._root_dir)
         pass
 
     @staticmethod
     def get_algorithm_class(path: str) -> Type[BaseDetector]:
-        pass
+        assert os.path.isfile(path), 'path is not an existing file'
+        assert os.path.splitext(path)[1] == '.py', 'path must be a python file'
+        classname: str = os.path.splitext(path)[0]
+        AlgorithmLoader.ensure_root_dir_in_path()
+        path_obj: pathlib.Path = pathlib.Path(path)
+        assert not path_obj.is_relative_to(AlgorithmLoader._root_dir), 'path must be contained in root dir'
+        import_path: tuple[str, ...] = path_obj.parent.relative_to(AlgorithmLoader._root_dir).parts
+        assert len(import_path) > 0, 'the file must not be directly in the root dir'
+        module: ModuleType= importlib.import_module('.'.join(import_path)+'.' + classname)
+        assert hasattr(module, classname), 'the file does not contain a python class of the same name'
+        return getattr(module, classname)
 
     @staticmethod
     def get_algorithm_object(path: str, parameters: Dict[str, object]) -> BaseDetector:
-        pass
+        return AlgorithmLoader.get_algorithm_class(path)(**parameters)
 
     @staticmethod
-    def is_algorithm_valid(path: str) -> str:
-        pass
+    def is_algorithm_valid(path: str) -> Optional[str]:
+        return None
 
     @staticmethod
     def get_algorithm_parameters(path: str) -> inspect.Signature.parameters:
-        pass
+        return inspect.signature(AlgorithmLoader.get_algorithm_class(path)).parameters
