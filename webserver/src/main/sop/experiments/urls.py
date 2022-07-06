4257f2868e1e61b3d425f834a7e70d26e25a562e
Jonas Bergmann <uhwkd@student.kit.edu>
2022-07-06 18:17:18 +0200

    Add redirect from algorithm/ to algorithm/overview

--- webserver/src/main/sop/experiments/urls.py
+++ webserver/src/main/sop/experiments/urls.py
@@ -1,20 +1,33 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
-from .views.algorithm import AlgorithmEditView, AlgorithmUploadView, \
-    AlgorithmDeleteView, AlgorithmOverview
 from experiments.views.dataset import DatasetOverview, DatasetDetailView
+from .views.algorithm import (
+    AlgorithmEditView,
+    AlgorithmUploadView,
+    AlgorithmDeleteView,
+    AlgorithmOverview,
+)
 
 urlpatterns = [
-    path('algorithm/overview/', AlgorithmOverview.as_view(),
+    path("algorithm/overview/", AlgorithmOverview.as_view(),
          name="algorithm_overview"),
-    path('algorithm/<int:pk>/delete/', AlgorithmDeleteView.as_view(),
-         name="algorithm_delete"),
-    path('algorithm/<int:pk>/edit/', AlgorithmEditView.as_view(),
-         name="algorithm_edit"),
-    path('algorithm/upload/', AlgorithmUploadView.as_view(),
+    path(
+        "algorithm/<int:pk>/delete/",
+        AlgorithmDeleteView.as_view(),
+        name="algorithm_delete",
+    ),
+    path(
+        "algorithm/<int:pk>/edit/", AlgorithmEditView.as_view(),
+        name="algorithm_edit"
+    ),
+    path("algorithm/upload/", AlgorithmUploadView.as_view(),
          name="algorithm_upload"),
-
+    path("algorithm/", RedirectView.as_view(pattern_name="algorithm_overview",
+                                            permanent=True)),
     # Dataset URLs
-    path('dataset/overview/', DatasetOverview.as_view(), name="dataset_overview"),
-    path('dataset/<int:pk>/', DatasetDetailView.as_view(), name="dataset_detail_view")
+    path("dataset/overview/", DatasetOverview.as_view(),
+         name="dataset_overview"),
+    path("dataset/<int:pk>/", DatasetDetailView.as_view(),
+         name="dataset_detail_view"),
 ]
