eb57823cbda63481752114493e618f280d9adcd0
Jonas-Bergmann <bergmann.jonas31@gmail.com>
2022-07-08 01:58:38 +0200

    Rename experiment model attributes

--- webserver/src/main/sop/experiments/models/experiment.py
+++ webserver/src/main/sop/experiments/models/experiment.py
@@ -14,42 +14,17 @@ class Experiment(models.Model):
     Database model of an experiment
     """
 
-    _display_name = models.CharField(max_length=80)
-    _user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=80)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     # We do not allow deletion of a dataset if it's used in an experiment
-    _dataset = models.ForeignKey(
+    dataset = models.ForeignKey(
         to=Dataset, on_delete=models.PROTECT, related_name="experiment"
     )
-    _algorithms = models.ManyToManyField(to=Algorithm)
-    _creation_date = models.DateTimeField(auto_now_add=True)
+    algorithms = models.ManyToManyField(to=Algorithm)
+    creation_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_together = ["_display_name", "_user"]
-
-    @property
-    def display_name(self) -> str:
-        return self._display_name
-
-    @display_name.setter
-    def display_name(self, value):
-        # TODO: maybe throw exception if value has more than 80 characters?
-        self._display_name = value
-
-    @property
-    def user(self) -> User:
-        return self._user
-
-    @property
-    def dataset(self) -> Dataset:
-        return self._dataset
-
-    @property
-    def algorithms(self) -> Manager:
-        return self._algorithms
-
-    @property
-    def creation_date(self) -> datetime:
-        return self._creation_date
+        unique_together = ["display_name", "user"]
 
     def __str__(self) -> str:
         return str(self.display_name)
