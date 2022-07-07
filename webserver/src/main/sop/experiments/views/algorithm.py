1dccf3700abc89adea520fdfd27e94c1eb87c7c4
Jonas Bergmann <uhwkd@student.kit.edu>
2022-07-07 17:20:23 +0200

    Reformat AlgorithmUploadView

--- webserver/src/main/sop/experiments/views/algorithm.py
+++ webserver/src/main/sop/experiments/views/algorithm.py
@@ -60,12 +60,7 @@ class AlgorithmUploadView(LoginRequiredMixin, CreateView):
     def form_valid(self, form) -> HttpResponse:
         temp_path: str = self.request.FILES["path"].temporary_file_path()
 
-        # save the contents of the uploaded file in a temporary file and check
-        # it for a valid implementation of BaseDetector
         error: Optional[str] = AlgorithmLoader.is_algorithm_valid(temp_path)
-
-        if error is None:
-            form.instance.signature = get_signature_of_algorithm(temp_path)
 
         if error is not None:
             # add the error to the form and display it as invalid
@@ -74,6 +69,7 @@ class AlgorithmUploadView(LoginRequiredMixin, CreateView):
 
         elif error is None:
             form.instance.user = self.request.user
+            form.instance.signature = get_signature_of_algorithm(temp_path)
             return super(AlgorithmUploadView, self).form_valid(form)
 
 
