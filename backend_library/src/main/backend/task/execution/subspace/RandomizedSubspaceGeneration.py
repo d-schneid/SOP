6efc9de9c4261ea6407bc4992737af49adb03693
Tobias <tobrohl@gmail.com>
2022-07-08 16:36:50 +0200

    make RandomizedSubspaceGeneration private variables really private

--- backend_library/src/main/backend/task/execution/subspace/RandomizedSubspaceGeneration.py
+++ backend_library/src/main/backend/task/execution/subspace/RandomizedSubspaceGeneration.py
@@ -14,16 +14,16 @@ from backend_library.src.main.backend.task.execution.subspace.SubspaceSizeDistri
 class RandomizedSubspaceGeneration(SubspaceGenerationDescription, ABC):
     def __init__(self, size_distr: SubspaceSizeDistribution, dataset_total_dimension_count: int, subspace_amount: int,
                  seed: int):
-        self._rnd: np.random.Generator = np.random.Generator(np.random.PCG64(seed))
+        self.__rnd: np.random.Generator = np.random.Generator(np.random.PCG64(seed))
         self.__ds_dim_count = dataset_total_dimension_count
-        self._size_distr: SubspaceSizeDistribution = size_distr
-        self._subspace_amount: int = subspace_amount
+        self.__size_distr: SubspaceSizeDistribution = size_distr
+        self.__subspace_amount: int = subspace_amount
 
     def to_json(self) -> string:
         pass
 
     def generate(self) -> Iterable[Subspace]:
-        size_counts: Dict[int, int] = self._size_distr.get_subspace_sizes(self._subspace_amount, self.__ds_dim_count)
+        size_counts: Dict[int, int] = self.__size_distr.get_subspace_sizes(self.__subspace_amount, self.__ds_dim_count)
         result: List[Subspace] = list()
         for k, v in size_counts.items():
             result.extend(self.__generate_subspaces_of_size(k, v))
@@ -35,7 +35,7 @@ class RandomizedSubspaceGeneration(SubspaceGenerationDescription, ABC):
         RandomizedSubspaceGeneration.__ensure_enough_subspaces(size, count, self.__ds_dim_count)
         current_mask: np.array = np.concatenate((np.repeat(True, size), np.repeat(False, self.__ds_dim_count - size)))
         while len(result) < count:
-            self._rnd.shuffle(current_mask)
+            self.__rnd.shuffle(current_mask)
             current_mask_bytes: bytes = np.packbits(current_mask).tobytes()
             if current_mask_bytes not in result_bytes:
                 result_bytes.append(current_mask_bytes)
