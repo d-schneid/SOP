6b9b2c112fb6c7b1bbb55d0f174e9f6694ddff9d
Daniel Schneider <daniel.schneider@student.kit.edu>
2022-07-08 15:48:11 +0200

    Refactor code, factor out temporary file and signature stuff for Algorithm model

--- webserver/src/main/sop/experiments/admin.py
+++ webserver/src/main/sop/experiments/admin.py
@@ -1,59 +1,23 @@
-import os
-import shutil
 from pathlib import Path
 from typing import Optional
 
-from django.core.files.uploadedfile import UploadedFile, TemporaryUploadedFile
+from django.core.files.uploadedfile import TemporaryUploadedFile
 from django import forms
 from django.contrib import admin
 
 from experiments.models import Algorithm, Dataset
-from authentication.models import User
 from backend.task.execution.AlgorithmLoader import AlgorithmLoader
 from sop.settings import MEDIA_ROOT
+from experiments.services import (
+    save_temp_algorithm,
+    delete_temp_algorithm,
+    get_signature_of_algorithm,
+)
 
 ALGORITHM_ROOT_DIR = MEDIA_ROOT / "algorithms"
 
 
 admin.site.register(Dataset)
-
-
-def save_temp_algorithm(user: User, file: UploadedFile):
-    temp_dir = ALGORITHM_ROOT_DIR / "temp" / f"{user.id}"
-    temp_file_path = temp_dir / file.name
-
-    if not os.path.exists(temp_dir):
-        os.makedirs(temp_dir)
-
-    # save contents of uploaded file into temp file
-    with open(temp_file_path, "wb") as temp_file:
-        for chunk in file.chunks():
-            temp_file.write(chunk)
-
-    return temp_file_path
-
-
-def delete_temp_algorithm(temp_file_path: Path):
-    parent_folder = temp_file_path.parent
-    assert temp_file_path.parent.parent == ALGORITHM_ROOT_DIR / "temp"
-
-    if not os.path.isdir(temp_file_path.parent):
-        return
-
-    # remove temp file
-    temp_file_path.unlink()
-
-    # remove parent dir if it has no files in it (ignore directories in it, since
-    # __pycache__ could have been created)
-    if not any([os.path.isfile(file) for file in os.listdir(parent_folder)]):
-        shutil.rmtree(parent_folder)
-
-
-def get_signature_of_algorithm(path: str) -> str:
-    algorithm_parameters = AlgorithmLoader.get_algorithm_parameters(path)
-    keys_values = algorithm_parameters.items()
-    string_dict = {key: str(value) for key, value in keys_values}
-    return ",".join(string_dict.values())
 
 
 class AlgorithmAddForm(forms.ModelForm):
---
+++ webserver/src/main/sop/experiments/services/algorithm.py
@@ -0,0 +1,49 @@
+import os
+import shutil
+from pathlib import Path
+
+from django.core.files.uploadedfile import UploadedFile
+
+from authentication.models import User
+from sop.settings import MEDIA_ROOT
+from backend.task.execution.AlgorithmLoader import AlgorithmLoader
+
+ALGORITHM_ROOT_DIR = MEDIA_ROOT / "algorithms"
+
+
+def save_temp_algorithm(user: User, file: UploadedFile):
+    temp_dir = ALGORITHM_ROOT_DIR / "temp" / f"{user.id}"
+    temp_file_path = temp_dir / file.name
+
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir)
+
+    # save contents of uploaded file into temp file
+    with open(temp_file_path, "wb") as temp_file:
+        for chunk in file.chunks():
+            temp_file.write(chunk)
+
+    return temp_file_path
+
+
+def delete_temp_algorithm(temp_file_path: Path):
+    parent_folder = temp_file_path.parent
+    assert temp_file_path.parent.parent == ALGORITHM_ROOT_DIR / "temp"
+
+    if not os.path.isdir(temp_file_path.parent):
+        return
+
+    # remove temp file
+    temp_file_path.unlink()
+
+    # remove parent dir if it has no files in it (ignore directories in it, since
+    # __pycache__ could have been created)
+    if not any([os.path.isfile(file) for file in os.listdir(parent_folder)]):
+        shutil.rmtree(parent_folder)
+
+
+def get_signature_of_algorithm(path: str) -> str:
+    algorithm_parameters = AlgorithmLoader.get_algorithm_parameters(path)
+    keys_values = algorithm_parameters.items()
+    string_dict = {key: str(value) for key, value in keys_values}
+    return ",".join(string_dict.values())
