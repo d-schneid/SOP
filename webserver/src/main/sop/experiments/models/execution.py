33aa5931597e2fa121f90061b9dab16255e3eb77
Jonas Bergmann <uhwkd@student.kit.edu>
2022-07-08 17:23:12 +0200

    Add seed to execution model

--- webserver/src/main/sop/experiments/models/execution.py
+++ webserver/src/main/sop/experiments/models/execution.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-import experiments.models
+from experiments.models.experiment import Experiment
 from experiments.models.managers import ExecutionManager, ExecutionQueryset
 
 
 class Execution(models.Model):
     experiment = models.ForeignKey(
-        to=experiments.models.Experiment, on_delete=models.CASCADE
+        to=Experiment, on_delete=models.CASCADE
     )
     status = models.CharField(max_length=80, default="created")
     progress = models.FloatField(default=0.0)
@@ -15,6 +15,7 @@ class Execution(models.Model):
     subspace_amount = models.IntegerField()
     subspaces_min = models.IntegerField()
     subspaces_max = models.IntegerField()
+    subspace_generation_seed = models.IntegerField(blank=True)
     algorithm_parameters = models.JSONField()
     result_path = models.FileField()
     objects = ExecutionManager.from_queryset(ExecutionQueryset)()
