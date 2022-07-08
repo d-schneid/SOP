3a6fb962fa70fe0ac096a94380621acdc94d0ce4
Jonas Bergmann <uhwkd@student.kit.edu>
2022-07-08 17:26:42 +0200

    Add experiments and executions to admin.py

--- webserver/src/main/sop/experiments/admin.py
+++ webserver/src/main/sop/experiments/admin.py
@@ -1,17 +1,21 @@
 from django.contrib import admin
 
-from experiments.models import Algorithm, Dataset
 from experiments.forms.admin import AdminAlgorithm
-
+from experiments.models.algorithm import Algorithm
+from experiments.models.dataset import Dataset
+from experiments.models.execution import Execution
+from experiments.models.experiment import Experiment
 
 admin.site.register(Dataset)
+admin.site.register(Experiment)
+admin.site.register(Execution)
 
 
 @admin.register(Algorithm)
 class AlgorithmAdmin(admin.ModelAdmin):
     form = AdminAlgorithm
-    list_display = ['name', 'group', 'description', "user"]
-    readonly_fields = ['upload_date']
+    list_display = ["name", "group", "description", "user"]
+    readonly_fields = ["upload_date"]
     raw_id_fields = ["user"]
     list_filter = ["name"]
     search_fields = ["group", "name"]
