ca932aa66721f91dc7c0bf6d231dd0998cd9727d
Jonas Bergmann <uhwkd@student.kit.edu>
2022-07-08 14:07:52 +0200

    Implement Execution model

--- webserver/src/main/sop/experiments/models/execution.py
+++ webserver/src/main/sop/experiments/models/execution.py
@@ -1,5 +1,20 @@
 from django.db import models
 
+import experiments.models
+from experiments.models.managers import ExecutionManager, ExecutionQueryset
+
 
 class Execution(models.Model):
-    pass
+    experiment = models.ForeignKey(
+        to=experiments.models.Experiment, on_delete=models.CASCADE
+    )
+    status = models.CharField(max_length=80, default="created")
+    progress = models.FloatField(default=0.0)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    finished_date = models.DateTimeField(blank=True, null=True)
+    subspace_amount = models.IntegerField()
+    subspaces_min = models.IntegerField()
+    subspaces_max = models.IntegerField()
+    algorithm_parameters = models.JSONField()
+    result_path = models.FileField()
+    objects = ExecutionManager.from_queryset(ExecutionQueryset)()
