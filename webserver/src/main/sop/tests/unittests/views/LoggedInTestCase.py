a9fc28ca1089931ba00fa2c3c807b861669965f4
Jonas Bergmann <uhwkd@student.kit.edu>
2022-07-09 20:37:41 +0200

    Implement custom LoggedInTestCase to make test cases that require a logged in user

---
+++ webserver/src/main/sop/tests/unittests/views/LoggedInTestCase.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from authentication.models import User
+
+
+class LoggedInTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.credentials = {
+            "username": "user",
+            "password": "passwd"
+        }
+        self.user = User.objects.create_user(**self.credentials)
+        self.client.post(reverse("login"), self.credentials, follow=True)
+        super().setUp()
