6aeb1dade851e715fc2f1b2866fd1a05a5bd69e6
Finn Ritter <Finn.Ritter@student.kit.edu>
2022-07-10 04:03:13 +0200

    Add property id to the User model
    The id is derived by hashing the unique username.

--- webserver/src/main/sop/authentication/models.py
+++ webserver/src/main/sop/authentication/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
@@ -6,5 +7,7 @@ class User(AbstractUser):
     Custom database model of a User.
     """
 
-    # User model can be extended
-    pass
+    @property
+    def id(self) -> int:
+        return hash(self.username)
+
