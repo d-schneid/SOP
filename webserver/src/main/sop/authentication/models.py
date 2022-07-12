fa1885803280713793a264b90e473f2061b36ca7
Finn Ritter <Finn.Ritter@student.kit.edu>
2022-07-12 10:29:55 +0200

    Delete property "id" in User model
    This propeprty is already defined by a superclass of user (with a different implementation).
    Furthermore, the guarantee that the id of an user is unique among all existing
    users is sufficient for the use case of the id and therefore no different (meaning
    different from both the perevious implementation and the implementation in the superclass)
    is needed.

--- webserver/src/main/sop/authentication/models.py
+++ webserver/src/main/sop/authentication/models.py
@@ -1,13 +1,7 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 
 class User(AbstractUser):
     """
     Custom database model of a User.
     """
-
-    @property
-    def id(self) -> int:
-        return hash(self.username)
-
