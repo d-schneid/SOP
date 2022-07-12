90233d6f8860c288d1bdd98289c9e0fa9e0a4b99
Tobias <tobrohl@gmail.com>
2022-07-12 18:22:51 +0200

    Document UniformSubspaceDistribution

--- backend_library/src/backend/task/execution/subspace/UniformSubspaceDistribution.py
+++ backend_library/src/backend/task/execution/subspace/UniformSubspaceDistribution.py
@@ -2,23 +2,45 @@ import math
 from abc import ABC
 from typing import Dict
 
-from backend.task.execution.subspace.SubspaceSizeDistribution import SubspaceSizeDistribution
+from backend.task.execution.subspace.SubspaceSizeDistribution \
+    import SubspaceSizeDistribution
 
 
 class UniformSubspaceDistribution(SubspaceSizeDistribution, ABC):
+    """A SubspaceSizeDistribution where there is almost the same number of Subspaces
+     for each Size in a range"""
+
     def __init__(self, subspace_size_min: int, subspace_size_max: int):
-        assert subspace_size_min <= subspace_size_max
+        """
+        Creates a new UniformSubspaceDistribution
+        :param subspace_size_min:  The minimum Size of Subspaces to create, inclusive
+        :param subspace_size_max:  The maximum Size of Subspaces to create, inclusive
+        :raises AssertionError when 0 < size_min <= size_max does not hold
+        """
+        assert 0 < subspace_size_min <= subspace_size_max
         self._subspace_size_min: int = subspace_size_min
         self._subspace_size_max: int = subspace_size_max
 
-    def get_subspace_sizes(self, requested_subspace_count: int, dataset_dimension_count: int) -> Dict[int, int]:
+    def get_subspace_sizes(self, requested_subspace_count: int,
+                           dataset_dimension_count: int) -> Dict[int, int]:
+        """
+        Calculates how many subspaces there are of each Size
+        :param requested_subspace_count: How may subspaces are needed
+        :param dataset_dimension_count: How many dimensions the dataset has
+        :raises AssertionError when size_max <= dataset_dimension_count or
+        requested_subspace_count > 0 does not hold
+        """
         assert self._subspace_size_max <= dataset_dimension_count
+        assert requested_subspace_count > 0
         size_count: int = self._subspace_size_max - self._subspace_size_min + 1
-        number_of_subspaces_per_size: int = math.floor(requested_subspace_count / size_count)
-        sizes_up_to_there_is_one_more_subspace: int = (requested_subspace_count % size_count) + self._subspace_size_min
+        number_of_subspaces_per_size: int = \
+            math.floor(requested_subspace_count / size_count)
+        sizes_with_one_more_subspace: int = \
+            (requested_subspace_count % size_count) + self._subspace_size_min
         result: Dict[int, int] = dict()
         for i in range(self._subspace_size_min, self._subspace_size_max + 1):
-            result[i] = number_of_subspaces_per_size + (1 if i < sizes_up_to_there_is_one_more_subspace else 0)
+            additional_subspaces = (1 if i < sizes_with_one_more_subspace else 0)
+            result[i] = number_of_subspaces_per_size + additional_subspaces
         return result
 
     def to_json(self) -> str:
