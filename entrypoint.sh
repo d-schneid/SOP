b6a2d82d43f3d86fd11789ea59fb3adfb0f4b27c
uuhmc <felix.zimmer@student.kit.edu>
2022-07-17 12:30:59 +0200

    Make gunicorn spawn 4 workers

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input
 python3 manage.py createsuperuser --no-input
 
-gunicorn sop.wsgi:application --bind 0.0.0.0:8000
+gunicorn sop.wsgi:application --bind 0.0.0.0:8000 --workers=4
