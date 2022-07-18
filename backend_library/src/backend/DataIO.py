adb0f1964ded98b8e9585631b9a82ea3a4ac309c
Finn Ritter <Finn.Ritter@student.kit.edu>
2022-07-19 01:28:10 +0200

    Integrate the automatic addition of a running suffix while the file is being written.
    This has the following advantages:
      - There is no need to specify an additional absolute path, which is very inconvenient.
      - The running suffix can be, but does not have to, be manually specified.
      - Current calls of the write_csv() method do not have to be changed (-> Backwards compatibility)
      - There is no confusion on which method should be called, as the running suffix should ALWAYS be added.

--- backend_library/src/backend/DataIO.py
+++ backend_library/src/backend/DataIO.py
@@ -1,4 +1,4 @@
-import shutil
+import os
 
 import numpy as np
 import pandas as pd
@@ -14,8 +14,7 @@ class DataIO:
         :return: The cleaned dataset (or an exception).
         """
         loaded_dataset = DataIO.read_uncleaned_csv(path)
-        cleaned_dataset: np.ndarray = loaded_dataset.astype(
-                np.float32)  # cast ndarray to float32
+        cleaned_dataset: np.ndarray = loaded_dataset.astype(np.float32)  # cast ndarray to float32
 
         return cleaned_dataset
 
@@ -45,42 +44,36 @@ class DataIO:
                     try:
                         to_convert[n, m] = float(to_convert[n, m])
                     except ValueError:
-                        continue  # leave value the same if it can't be casted
+                        continue  # leave value the same if it can't be cast
         else:  # edge case: only one row
             for n in range(0, to_convert.shape[0]):
                 try:
                     to_convert[n] = float(to_convert[n])
                 except ValueError:
-                    continue  # leave value the same if it can't be casted
+                    continue  # leave value the same if it can't be cast
         return to_convert
 
     @staticmethod
-    def write_csv(path: str, data: np.ndarray, add_index_column: bool = False):
+    def write_csv(path: str, data: np.ndarray, add_index_column: bool = False, running_suffix: str = ".running"):
         """
-        Writes the given 2D-dataset to a csv-file. \n
+        Writes the given 2D-dataset to a csv-file.
+
+        While the data is written to the file, a suffix (by default: ".running") is added at the end of the path,
+        which is renamed after the writing has finished,
+        so that a corrupted file due to a server crash can be detected.
+
         :param path: The absolute path to the location of the csv-file to be created and written to.
-        :param data: The dataset that should be created and written to.
+        :param data: The dataset that should be written to the file.
         :param add_index_column: If True create an additional column at the start of the array with
         indexes for each row. If False don't change anything.
-        :data: The dataset to write to the file.
+        :param running_suffix: Specifies the suffix to be added to the file during writing.
         """
+
+        temp_path: str = path + running_suffix
 
         df = pd.DataFrame(data)
         assert len(df.shape) == 2
 
-        df.to_csv(path, index=add_index_column)
+        df.to_csv(temp_path, index=add_index_column)
 
-    @staticmethod
-    def save_write_csv(running_path: str, final_path: str, data: np.ndarray, add_index_column: bool = False):
-        """
-        Write csv first at running path (e.g. ends with .running) and renames the file after writing
-        it to the final path.
-        :param running_path: The absolute path where to write the csv file to before renaming it.
-        :param final_path: The absolute path where the final written csv file will be stored.
-        :param data: The dataset that should be created and written to.
-        :param add_index_column: If True create an additional column at the start of the array with
-        indexes for each row. If False don't change anything.
-        :return:
-        """
-        DataIO.write_csv(running_path, data, add_index_column)
-        shutil.move(running_path, final_path)
+        os.rename(temp_path, path)  # # is a atomic operation (POSIX requirement)
