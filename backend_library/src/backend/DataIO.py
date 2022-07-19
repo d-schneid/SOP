afa369ce7f5b27aa5d8f1d86f3c461217f9e6098
Merlin Opp <ufjqw@student.kit.edu>
2022-07-19 22:37:36 +0200

    Reimport save_write_csv that @Finn deleted. Resolve rebase conflicts 2/n

--- backend_library/src/backend/DataIO.py
+++ backend_library/src/backend/DataIO.py
@@ -111,3 +111,17 @@ class DataIO:
         shutil.move(path_running, path_final)
 
 
+    @staticmethod
+    def save_write_csv(running_path: str, final_path: str, data: np.ndarray, add_index_column: bool = False):
+        """
+        Write csv first at running path (e.g. ends with .running) and renames the file after writing
+        it to the final path.
+        :param running_path: The absolute path where to write the csv file to before renaming it.
+        :param final_path: The absolute path where the final written csv file will be stored.
+        :param data: The dataset that should be created and written to.
+        :param add_index_column: If True create an additional column at the start of the array with
+        indexes for each row. If False don't change anything.
+        :return:
+        """
+        DataIO.write_csv(running_path, data, add_index_column)
+        shutil.move(running_path, final_path)
