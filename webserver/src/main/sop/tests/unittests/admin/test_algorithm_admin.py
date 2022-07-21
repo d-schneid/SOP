7ecfc70f46e12f1a6292c8fa3bd389e962b024ce
Daniel Schneider <daniel.schneider@student.kit.edu>
2022-07-21 19:32:26 +0200

    Add tests for deletion of algorithms in admin panel

--- webserver/src/main/sop/tests/unittests/admin/test_algorithm_admin.py
+++ webserver/src/main/sop/tests/unittests/admin/test_algorithm_admin.py
@@ -3,29 +3,33 @@ from django.urls import reverse
 
 from experiments.admin.algorithm import ExperimentInline
 from experiments.models.algorithm import Algorithm
+from experiments.models.experiment import Experiment
+from experiments.models.dataset import Dataset
 from tests.unittests.views.LoggedInTestCase import AdminLoggedInTestCase
 
 
+class MockRequest:
+    pass
+
+request = MockRequest()
+
 class ExperimentInlineTests(AdminLoggedInTestCase):
-    class MockRequest:
-        pass
 
     def setUp(self):
         super().setUp()
-        self.request = self.MockRequest()
-        self.request.user = self.admin
+        request.user = self.admin
         self.site = AdminSite()
         self.experiment_inline = ExperimentInline(Algorithm, self.site)
 
     def test_experiment_inline_add_permission(self):
         algorithm = Algorithm.objects.create(signature="")
-        self.assertEqual(self.experiment_inline.has_add_permission(self.request, algorithm), False)
+        self.assertEqual(self.experiment_inline.has_add_permission(request, algorithm), False)
 
     def test_experiment_inline_change_permission(self):
-        self.assertEqual(self.experiment_inline.has_change_permission(self.request), False)
+        self.assertEqual(self.experiment_inline.has_change_permission(request), False)
 
     def test_experiment_inline_delete_permission(self):
-        self.assertEqual(self.experiment_inline.has_change_permission(self.request), False)
+        self.assertEqual(self.experiment_inline.has_change_permission(request), False)
 
     def test_experiment_inline_verbose_name(self):
         self.assertEqual(self.experiment_inline.verbose_name, "Experiment")
@@ -47,11 +51,9 @@ def upload_algorithm(client, name, group, description, file_name):
 
 
 class AlgorithmAdminTests(AdminLoggedInTestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.algorithm = Algorithm.objects.create(
-            signature="",
-        )
+    def setUp(self) -> None:
+        super().setUp()
+        self.algo = Algorithm.objects.create(display_name="Test Algo", signature="")
 
     def test_algorithm_admin_changelist_view(self):
         url = reverse("admin:experiments_algorithm_changelist")
@@ -64,11 +66,23 @@ class AlgorithmAdminTests(AdminLoggedInTestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_algorithm_admin_change_view(self):
-        url = reverse("admin:experiments_algorithm_change", args=(self.algorithm.pk,))
+        url = reverse("admin:experiments_algorithm_change", args=(self.algo.pk,))
         response = self.client.get(url, follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "experiment_inline.html")
         self.assertContains(response, "Usage in experiments")
+
+    def test_algorithm_admin_delete_view(self):
+        self.assertTrue(Algorithm.objects.exists())
+        # confirm deletion on confirmation site
+        data = {"post": "yes"}
+        url = reverse("admin:experiments_algorithm_delete", args=(self.algo.pk,))
+        response = self.client.post(url, data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, f"{self.algo.display_name}")
+        self.assertContains(response, "was deleted successfully")
+        self.assertFalse(Algorithm.objects.exists())
+        self.assertEqual(response.resolver_match.url_name, "experiments_algorithm_changelist")
 
     def test_admin_add_valid_algorithm(self):
         test_name = "Test Valid Algorithm"
@@ -92,3 +106,18 @@ class AlgorithmAdminTests(AdminLoggedInTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.resolver_match.url_name, "experiments_algorithm_add")
         self.assertContains(response, "This is not a valid algorithm")
+
+    def test_action_delete_selected(self):
+        Algorithm.objects.create(signature="")
+        self.assertTrue(Algorithm.objects.exists())
+        algo_queryset = Algorithm.objects.all()
+        data = {"action": "delete_selected",
+                "_selected_action": [algo.pk for algo in algo_queryset],
+                # confirm deletion on confirmation site
+                "post": "yes"}
+        url = reverse("admin:experiments_algorithm_changelist")
+        response = self.client.post(url, data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, f"Successfully deleted {algo_queryset.count()} algorithms")
+        self.assertFalse(Algorithm.objects.exists())
+        self.assertEqual(response.resolver_match.url_name, "experiments_algorithm_changelist")
