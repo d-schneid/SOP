c0dfc9ba41e7ab194c8238b5bcf1036cbf68f52d
Merlin Opp <ufjqw@student.kit.edu>
2022-07-22 13:56:30 +0200

    Implement MetricSubspaceOutlierAmount

--- backend_library/src/backend/metric/MetricSubspaceOutlierAmount.py
+++ backend_library/src/backend/metric/MetricSubspaceOutlierAmount.py
@@ -19,56 +19,31 @@ class MetricSubspaceOutlierAmount(ABC, Metric):
         """
         assert metric_result_path.endswith(".csv")
 
+        # Get all .csv files (ExecutionElement result)
         execution_result_paths: list[str] = eem_helper. \
             get_execution_elements_result_paths(execution.algorithm_directory_paths)
 
-        outlier_data_points: list[np.ndarray] = list([])
-
-        paths_divided_in_subspaces: dict = {}
+        # Get subspace identifier
+        outlier_data_points_divided_in_subspaces: dict = {}
         all_subspace_identifier: list[str] = \
             eem_helper.convert_paths_into_subspace_identifier(execution_result_paths)
 
-        # Divide paths by their identifier
+        # Divide outlier_data_points by their subspace identifier
         for identifier in all_subspace_identifier:
             for path in execution_result_paths:
-                if (paths_divided_in_subspaces.get(identifier)) is None:
-                    paths_divided_in_subspaces[identifier] = list([path])
+                if (outlier_data_points_divided_in_subspaces.get(identifier)) is None:
+                    outlier_data_points_divided_in_subspaces[identifier] \
+                        = list([eem_helper.compute_outlier_data_points(path)])
                 else:
-                    paths_divided_in_subspaces[identifier].append(path)
+                    outlier_data_points_divided_in_subspaces[identifier]\
+                        .append(eem_helper.compute_outlier_data_points(path))
 
+        # compute metric
+        outlier_data_points: list[int] = \
+            eem_helper.compute_subspace_outlier_amount(outlier_data_points_divided_in_subspaces)
 
+        # convert into result
+        metric_result: np.ndarray = np.hstack(all_subspace_identifier, outlier_data_points)
 
-
-        #compute_data_point_outlier_count für jede subspace einzeln!!
-        # split execution_result_path into the subspaces
-
-
-
-        # Fill outlier_data_points with all information about which datapoint is an outlier
-        # (1 bool array for each ExecutionElement result)
-        for result_path in execution_result_path:
-            outlier_data_points.append(eem_helper.compute_outlier_data_points(result_path))
-
-
-
-        # Partition outlier_data_points into same subspace
-        _subspace_amount: int = len(list(execution.subspaces))
-
-
-
-
-
-        # Compute the metric result:
-
-
-
-
-
-
-
-        subspace_outlier_amount: list[int] = eem_helper.compute_subspace_outlier_amount()
-
-        # Create fist column of the array: The subspace identifier
-
-        # save metric
-        DataIO.save_write_csv(metric_result_path+".running", metric_result_path, TODO)
+        # save metric result
+        DataIO.save_write_csv(metric_result_path+".running", metric_result_path, metric_result)
