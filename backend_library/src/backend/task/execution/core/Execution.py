c21fb526a9e7c3ce4df660d0595b978768c17971
Tobias <tobrohl@gmail.com>
2022-07-22 21:39:13 +0200

    Fix us forgetting to execute the task_progress_callback

--- backend_library/src/backend/task/execution/core/Execution.py
+++ backend_library/src/backend/task/execution/core/Execution.py
@@ -3,29 +3,25 @@ from __future__ import annotations
 import json
 import multiprocessing
 import os
-import sys
-
+from collections.abc import Iterable
 from multiprocessing import shared_memory
-from abc import ABC
-from multiprocessing.shared_memory import SharedMemory
 from typing import Callable, Optional
 from typing import List
-from collections.abc import Iterable
 
 import numpy as np
 
 from backend.DataIO import DataIO
 from backend.scheduler.Schedulable import Schedulable
+from backend.scheduler.Scheduler import Scheduler
 from backend.task.Task import Task
-from backend.task.TaskState import TaskState
 from backend.task.TaskHelper import TaskHelper
+from backend.task.TaskState import TaskState
+from backend.task.execution.ParameterizedAlgorithm import ParameterizedAlgorithm
+from backend.task.execution.ResultZipper import ResultZipper
+from backend.task.execution.core import ExecutionSubspace
+from backend.task.execution.subspace.Subspace import Subspace
 from backend.task.execution.subspace.SubspaceGenerationDescription import \
     SubspaceGenerationDescription
-from backend.task.execution.core import ExecutionSubspace
-from backend.scheduler.Scheduler import Scheduler
-from backend.task.execution.ResultZipper import ResultZipper
-from backend.task.execution.ParameterizedAlgorithm import ParameterizedAlgorithm
-from backend.task.execution.subspace.Subspace import Subspace
 
 
 class Execution(Task, Schedulable):
@@ -88,7 +84,8 @@ class Execution(Task, Schedulable):
         # generate subspaces
         self._subspaces: List[Subspace] = list(self._subspace_generation.generate())
         self._subspaces_count = len(self._subspaces)
-        self._total_execution_element_count: int = self._subspaces_count * len(self._algorithms)
+        self._total_execution_element_count: int = self._subspaces_count * len(
+            self._algorithms)
 
         # generate execution_subspaces
         self._execution_subspaces: List[ExecutionSubspace] = list()
@@ -116,7 +113,8 @@ class Execution(Task, Schedulable):
                 algorithm_display_name_dict[algorithm.display_name] = 1
             else:
                 algorithm.directory_name_in_execution = display_name + " (" \
-                                                        + str(algorithm_display_name_dict[display_name]) + ")"
+                                                        + str(
+                    algorithm_display_name_dict[display_name]) + ")"
                 algorithm_display_name_dict[algorithm.display_name] += 1
 
     # Generates all missing folders of the file system structure of this execution
@@ -129,7 +127,8 @@ class Execution(Task, Schedulable):
         if not os.path.isdir(self._result_path):
             TaskHelper.create_directory(self._result_path)
             for algorithm in self._algorithms:
-                algorithm_directory_path: str = os.path.join(self._result_path, algorithm.directory_name_in_execution)
+                algorithm_directory_path: str = os.path.join(self._result_path,
+                                                             algorithm.directory_name_in_execution)
                 TaskHelper.create_directory(algorithm_directory_path)
 
     def __generate_execution_details_in_filesystem(self) -> None:
@@ -143,7 +142,8 @@ class Execution(Task, Schedulable):
         details_path: str = os.path.join(self._result_path, 'details.json')
 
         # create dictionary that will be saved as a JSON-str
-        details_dict = {'subspace_generation_information': self._subspace_generation.to_json()}
+        details_dict = {
+            'subspace_generation_information': self._subspace_generation.to_json()}
         for algorithm in self._algorithms:
             algorithm: ParameterizedAlgorithm = algorithm  # To get the type hint
             details_dict[algorithm.directory_name_in_execution] = algorithm.to_json()
@@ -192,10 +192,12 @@ class Execution(Task, Schedulable):
         if a Task is finished. \n
         :return: A float in [0,1] which indicates the progress of the Execution.
         """
-        execution_element_progress: float = float(self._finished_execution_element_count) \
+        execution_element_progress: float = float(
+            self._finished_execution_element_count) \
                                             / float(self._total_execution_element_count)
         execution_element_progress *= 0.98  # So all progress is shown (so that the clamping doesn't remove information)
-        progress: float = max(0., min(execution_element_progress, 0.98))  # clamp the progress to be more accurate
+        progress: float = max(0., min(execution_element_progress,
+                                      0.98))  # clamp the progress to be more accurate
         if self._metric_finished:
             progress += 0.01
         # Note: The 100% progress is only reached after zipping
@@ -221,17 +223,26 @@ class Execution(Task, Schedulable):
         """
         if not self._has_failed_element and error:
             self._has_failed_element = True
+        assert self._finished_execution_element_count < \
+               self._total_execution_element_count, \
+            "More execution elements finished than existing"
 
-        if self._finished_execution_element_count < self._total_execution_element_count:
-            with self._execution_element_finished_lock:
-                self._finished_execution_element_count += 1
-                if self._finished_execution_element_count == self._total_execution_element_count:
-                    self.__unload_dataset()
-                    self._metric_callback(self)
-                    self._metric_finished = True
-                    self.__schedule_result_zipping()
-        else:
-            raise AssertionError("More execution elements finished than existing")
+        with self._execution_element_finished_lock:
+            self._finished_execution_element_count += 1
+            self.__run_progress_callback()
+            if self._finished_execution_element_count == \
+                    self._total_execution_element_count:
+                self.__unload_dataset()
+                self._metric_callback(self)
+                self._metric_finished = True
+                self.__run_progress_callback()
+                self.__schedule_result_zipping()
+
+    def __run_progress_callback(self):
+        """Executes the task progress callback with the appropriate parameters"""
+        state = TaskState.RUNNING_WITH_ERROR if self._has_failed_element else \
+            TaskState.RUNNING
+        self._task_progress_callback(self.task_id, state, self.__compute_progress())
 
     def __unload_dataset(self) -> None:
         """
@@ -250,9 +261,12 @@ class Execution(Task, Schedulable):
         Create and schedule the ResultZipping of the Execution into the Scheduler. \n
         :return: None
         """
-        result_zipper: ResultZipper = ResultZipper(self._user_id, self._task_id, self._has_failed_element,
-                                                   self._task_progress_callback, self._result_path,
-                                                   self._zip_running_path, self._final_zip_path)
+        result_zipper: ResultZipper = ResultZipper(self._user_id, self._task_id,
+                                                   self._has_failed_element,
+                                                   self._task_progress_callback,
+                                                   self._result_path,
+                                                   self._zip_running_path,
+                                                   self._final_zip_path)
         scheduler: Scheduler = Scheduler.get_instance()
         scheduler.schedule(result_zipper)
 
