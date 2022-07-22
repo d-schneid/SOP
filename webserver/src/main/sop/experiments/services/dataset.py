1d2a7722b12f6a2a64f7a8e8aca2bee83b184e81
Finn Ritter <Finn.Ritter@student.kit.edu>
2022-07-22 21:49:57 +0200

    Add .csv-ending to dataset-paths

--- webserver/src/main/sop/experiments/services/dataset.py
+++ webserver/src/main/sop/experiments/services/dataset.py
@@ -14,11 +14,11 @@ def check_if_file_is_csv(path: str) -> bool:
 
 def generate_path_dataset_uncleaned_and_move_dataset(temp_path: str, user_id: int, dataset_id: int) -> str:
     final_path: str = os.path.join(DATASET_ROOT_DIR, "user_" + str(user_id),
-                                   "dataset_" + str(dataset_id)  + "_uncleaned")
+                                   "dataset_" + str(dataset_id)  + "_uncleaned.csv")
     shutil.move(temp_path, final_path)
     return final_path
 
 
 def generate_path_dataset_cleaned(user_id: int, dataset_id: int) -> str:
     return os.path.join(DATASET_ROOT_DIR, "user_" + str(user_id),
-                                   "dataset_" + str(dataset_id) + "_cleaned")
+                                   "dataset_" + str(dataset_id) + "_cleaned.csv")
