933606c522b0a701f95dd96d603fa98a56df3fa9
Daniel Schneider <daniel.schneider@student.kit.edu>
2022-07-26 00:48:45 +0200

    Dynamically load read-only fields

--- webserver/src/main/sop/experiments/admin/experiment.py
+++ webserver/src/main/sop/experiments/admin/experiment.py
@@ -8,8 +8,15 @@ class ExperimentAdmin(admin.ModelAdmin[Experiment]):
     list_display = ["display_name", "user", "creation_date"]
     raw_id_fields = ["user"]
     list_filter = ["creation_date"]
-    readonly_fields = ["user", "dataset", "algorithms"]
     search_fields = ["display_name",
                      "user__username",
                      "algorithms__display_name",
                      "dataset__display_name"]
+
+    def get_readonly_fields(self, request, obj=None):
+        # for editing an existing experiment
+        if obj:
+            return ["user", "dataset", "algorithms"]
+        # for adding a new experiment
+        else:
+            return []
