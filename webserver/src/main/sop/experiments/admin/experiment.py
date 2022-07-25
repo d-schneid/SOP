4ad0492193dab103ddff79fbe710822df4b83e17
Daniel Schneider <daniel.schneider@student.kit.edu>
2022-07-26 00:19:27 +0200

    Implement overview of ExperimentAdmin

--- webserver/src/main/sop/experiments/admin/experiment.py
+++ webserver/src/main/sop/experiments/admin/experiment.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from experiments.models.experiment import Experiment
 
-admin.site.register(Experiment)
+
+@admin.register(Experiment)
+class ExperimentAdmin(admin.ModelAdmin[Experiment]):
+    list_display = ["display_name", "user", "creation_date"]
+    raw_id_fields = ["user"]
+    list_filter = ["creation_date"]
+    search_fields = ["display_name",
+                     "user__username",
+                     "algorithms__display_name",
+                     "dataset__display_name"]
