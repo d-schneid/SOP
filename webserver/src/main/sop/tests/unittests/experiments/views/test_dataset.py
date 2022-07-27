fb95711f4c3bc42709371c249b3fdd82913beb16
Jonas Bergmann <uhwkd@student.kit.edu>
2022-07-27 16:18:18 +0200

    Add another dataset view test

--- webserver/src/main/sop/tests/unittests/experiments/views/test_dataset.py
+++ webserver/src/main/sop/tests/unittests/experiments/views/test_dataset.py
@@ -114,3 +114,16 @@ class DatasetViewTests(django.test.TestCase):
             assert response is not None
             self.assertEqual(response.status_code, 302)
             self.assertEqual(response.url, reverse_lazy("dataset_overview"))  # type: ignore
+
+    def test_download_cleaned_dataset_post(self) -> None:
+        dataset = MagicMock()
+        dataset.display_name = "dataset_name"
+        dataset.is_cleaned = False
+        dataset.pk = 3
+        object_mock = MagicMock()
+        object_mock.filter.return_value.first.return_value = dataset
+        request = MagicMock()
+        request.method = "POST"
+        with mock.patch.object(Dataset, "objects", object_mock):
+            response = download_cleaned_dataset(request, 42)
+            self.assertIsNone(response)
