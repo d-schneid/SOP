2b4bd7ab6917ae725955ebb689c2e9483982fa7b
Felix Zimmer <felix.zimmer@student.kit.edu>
2022-07-30 13:34:19 +0200

    Add error reporting to login view

--- webserver/src/main/sop/authentication/views.py
+++ webserver/src/main/sop/authentication/views.py
@@ -1,5 +1,8 @@
+from django.contrib import messages
 from django.contrib.auth.views import LoginView as DjangoLoginView
 from django.contrib.auth.views import LogoutView as DjangoLogoutView
+from django.forms import Form
+from django.http import HttpResponse, HttpRequest
 
 from authentication.forms import LoginForm
 
@@ -10,6 +13,18 @@ class LoginView(DjangoLoginView):
     """
 
     form_class = LoginForm
+
+    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        form = self.get_form()
+
+        # Check is form is invalid and set error messages
+        if not form.is_valid():
+            error_text = ' '.join(
+                [error_message for error in form.errors.values() for error_message in error])
+
+            messages.error(request, f"Invalid Login: {error_text}")
+        return super().post(request, *args, **kwargs)
+    # def form_valid(self, form: Form) -> HttpResponse:
 
 
 class LogoutView(DjangoLogoutView):
