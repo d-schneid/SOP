10ed5b7f98c1cec32b6ba4ca9cadf2af94fd7b30
Merlin Opp <ufjqw@student.kit.edu>
2022-07-31 14:53:49 +0200

    Add edge case test for MetricDataPointsAreOutlier with wrong indices

--- backend_library/src/test/integration_tests/backend/metric/test_MetricDataPointsAreOutliers.py
+++ backend_library/src/test/integration_tests/backend/metric/test_MetricDataPointsAreOutliers.py
@@ -39,6 +39,7 @@ class IntegrationTest_MetricDataPointsAreOutlier(unittest.TestCase):
     _metric_other_indices: list[int] = list([3, 15415, -142, 12, 45, 8, 51, 999,
                                              -124, 5, 10, 11, 12, 13, 14, 15, 16,
                                              17, 18, 19, 192143])
+    _metric_not_enough_indices: list[int] = list([42])
 
     def setUp(self) -> None:
         self.__clean_up_files()
@@ -46,6 +47,8 @@ class IntegrationTest_MetricDataPointsAreOutlier(unittest.TestCase):
         self._metric = MetricDataPointsAreOutliers(self._metric_default_indices)
         self._other_indices_metric = \
             MetricDataPointsAreOutliers(self._metric_other_indices)
+        self._not_enough_indices_metric = \
+            MetricDataPointsAreOutliers(self._metric_not_enough_indices)
 
         for algorithm_path in self._algorithm_directory_paths:
             self.assertTrue(os.path.isdir(algorithm_path))
@@ -72,6 +75,20 @@ class IntegrationTest_MetricDataPointsAreOutlier(unittest.TestCase):
         # clean up
         self.__clean_up_files()
 
+    def test_not_enough_indices(self):
+        # compute metric of execution example folder
+        self._other_indices_metric.compute_metric(self._metric_result_path1,
+                                                  self._algorithm_directory_paths)
+
+        # Indices amount doesn't match with data points amount -> AssertionError
+        with self.assertRaises(AssertionError) as context:
+            self._not_enough_indices_metric.\
+                compute_metric(self._metric_result_path1,
+                               self._algorithm_directory_paths)
+
+        # clean up
+        self.__clean_up_files()
+
     def test_wrong_metric_path(self):
         # Path doesn't end with .csv
         with self.assertRaises(AssertionError) as context:
@@ -85,6 +102,7 @@ class IntegrationTest_MetricDataPointsAreOutlier(unittest.TestCase):
             os.remove(self._metric_result_path1)
         if os.path.isfile(self._metric_result_path2):
             os.remove(self._metric_result_path2)
+
 
 if __name__ == '__main__':
     unittest.main()
