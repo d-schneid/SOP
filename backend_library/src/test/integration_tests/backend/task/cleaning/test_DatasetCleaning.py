f92bc7c5ba5bf52fa15f89e254e8df4ace479a7d
Merlin Opp <ufjqw@student.kit.edu>
2022-08-02 21:55:48 +0200

    Fix DataCleaning integration tests

--- backend_library/src/test/integration_tests/backend/task/cleaning/test_DatasetCleaning.py
+++ backend_library/src/test/integration_tests/backend/task/cleaning/test_DatasetCleaning.py
@@ -30,8 +30,10 @@ class IntegrationTestDatasetCleaning1(unittest.TestCase):
     def setUp(self) -> None:
         with open(self._uncleaned_dataset_path, 'w') as uncleaned_csv:
             self._dc: DatasetCleaning = DatasetCleaning(self._user_id, self._task_id,
-                                                        self.task_progress_callback, self._uncleaned_dataset_path,
-                                                        self._cleaned_dataset_path, iter([]), self._priority)
+                                                        self.task_progress_callback,
+                                                        self._uncleaned_dataset_path,
+                                                        self._cleaned_dataset_path,
+                                                        iter([]), self._priority)
         self._finished_cleaning = False
 
     def tearDown(self) -> None:
@@ -61,7 +63,8 @@ class IntegrationTestDatasetCleaning1(unittest.TestCase):
             self.assertTrue(self._finished_cleaning)
         os.remove(self._cleaned_dataset_path)
 
-    def task_progress_callback(self, _task_id: int, task_state: TaskState, progress: float) -> None:
+    def task_progress_callback(self, _task_id: int, task_state: TaskState,
+                               progress: float) -> None:
         if task_state.is_finished():
             self._finished_cleaning = True
 
@@ -78,11 +81,12 @@ class IntegrationTestDatasetCleaningNoUncleanedDataset(unittest.TestCase):
 
     def setUp(self) -> None:
         with open(self._uncleaned_dataset_path, 'w') as uncleaned_csv:
-            self._dc_missing_uncleaned_dataset: DatasetCleaning = DatasetCleaning(self._user_id, self._task_id,
-                                                                                  self.task_progress_callback,
-                                                                                  "no_uncleaned_dataset",
-                                                                                  self._cleaned_dataset_path, iter([]),
-                                                                                  self._priority)
+            self._dc_missing_uncleaned_dataset: DatasetCleaning = DatasetCleaning(
+                self._user_id, self._task_id,
+                self.task_progress_callback,
+                "no_uncleaned_dataset",
+                self._cleaned_dataset_path, iter([]),
+                self._priority)
 
     def tearDown(self) -> None:
         if os.path.isfile(self._uncleaned_dataset_path):
@@ -96,14 +100,16 @@ class IntegrationTestDatasetCleaningNoUncleanedDataset(unittest.TestCase):
         with self.assertRaises(AssertionError) as context:
             self._dc_missing_uncleaned_dataset._DatasetCleaning__load_uncleaned_dataset()
 
-    def task_progress_callback(self, _task_id: int, task_state: TaskState, progress: float) -> None:
+    def task_progress_callback(self, _task_id: int, task_state: TaskState,
+                               progress: float) -> None:
         pass
 
 
 class IntegrationTestDatasetCleaningRunCleaningPipeline(unittest.TestCase):
     _dir_name: str = os.getcwd()
     # dataset 1
-    _uncleaned_dataset_path: str = os.path.join(_dir_name, "uncleaned_dataset1.csv.error")
+    _uncleaned_dataset_path: str = os.path.join(_dir_name,
+                                                "uncleaned_dataset1.csv.error")
     _cleaned_dataset_path: str = os.path.join(_dir_name, "cleaned_dataset1.csv")
 
     _uncleaned_dataset1: np.ndarray = ds().cat_dataset3
@@ -118,17 +124,22 @@ class IntegrationTestDatasetCleaningRunCleaningPipeline(unittest.TestCase):
     _task_id: int = -1
     _priority: int = 9999
 
-    def task_progress_callback(self, _task_id: int, task_state: TaskState, progress: float) -> None:
+    def task_progress_callback(self, _task_id: int, task_state: TaskState,
+                               progress: float) -> None:
         self.assertTrue(task_state.is_running())
         self.assertTrue(progress >= 0)
         self.assertTrue(progress < 1)  # Is smaller than one in run_pipeline
 
     def setUp(self) -> None:
+        self._ds = ds()
+
         self.__clean_created_files_and_directories()
         with open(self._uncleaned_dataset_path, 'w') as uncleaned_csv:
             self._dc: DatasetCleaning = DatasetCleaning(self._user_id, self._task_id,
-                                                         self.task_progress_callback, self._uncleaned_dataset_path,
-                                                         self._cleaned_dataset_path, None, self._priority)
+                                                        self.task_progress_callback,
+                                                        self._uncleaned_dataset_path,
+                                                        self._cleaned_dataset_path,
+                                                        None, self._priority)
 
     def __clean_created_files_and_directories(self):
         if os.path.isfile(self._cleaned_dataset_path):
@@ -140,19 +151,25 @@ class IntegrationTestDatasetCleaningRunCleaningPipeline(unittest.TestCase):
 
     def test_run_cleaning_pipeline1(self):
         np.testing.assert_array_equal(self._cleaned_dataset1,
-                                      self._dc._DatasetCleaning__run_cleaning_pipeline(self._uncleaned_dataset1))
+                                      self._dc._DatasetCleaning__run_cleaning_pipeline
+                                      (self._ds.data_to_annotated
+                                       (self._uncleaned_dataset1)).data)
 
     def test_run_cleaning_pipeline2(self):
-        cleaned_dataset2: np.ndarray = np.asarray([[0.00000000e+00, 1.00000000e+00, 0.00000000e+00, 1.00000000e+00,
-                                                    0.00000000e+00, 1.00000000e+00, 1.00000000e+00],
-                                                   [1.00000000e+00, 5.00405186e-01, 1.00000000e+00, 0.00000000e+00,
-                                                    8.12019199e-02, 1.32896857e-06, 0.00000000e+00],
-                                                   [6.07124844e-05, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00,
-                                                    1.00000000e+00, 0.00000000e+00, 9.87870182e-01],
-                                                   [6.07124844e-05, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00,
-                                                    1.00000000e+00, 0.00000000e+00, 9.87870182e-01]])
-        np.testing.assert_array_almost_equal(cleaned_dataset2, self._dc._DatasetCleaning__run_cleaning_pipeline(
-            self._uncleaned_dataset2))
+        cleaned_dataset2: np.ndarray = np.asarray(
+            [[0.00000000e+00, 1.00000000e+00, 0.00000000e+00, 1.00000000e+00,
+              0.00000000e+00, 1.00000000e+00, 1.00000000e+00],
+             [1.00000000e+00, 5.00405186e-01, 1.00000000e+00, 0.00000000e+00,
+              8.12019199e-02, 1.32896857e-06, 0.00000000e+00],
+             [6.07124844e-05, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00,
+              1.00000000e+00, 0.00000000e+00, 9.87870182e-01],
+             [6.07124844e-05, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00,
+              1.00000000e+00, 0.00000000e+00, 9.87870182e-01]])
+        np.testing.assert_array_almost_equal(cleaned_dataset2,
+                                             self._dc
+                                             ._DatasetCleaning__run_cleaning_pipeline
+                                                 (self._ds.data_to_annotated(
+                                                 self._uncleaned_dataset2)).data)
 
 
 if __name__ == '__main__':
