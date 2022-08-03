c84a1497caef125eca9fc970a978a35b0094c8d6
Jonas-Bergmann <bergmann.jonas31@gmail.com>
2022-08-03 13:17:43 +0200

    Add pydocs to experiments.services.execution

--- webserver/src/main/sop/experiments/services/execution.py
+++ webserver/src/main/sop/experiments/services/execution.py
@@ -11,6 +11,18 @@ from experiments.models.algorithm import HyperparameterTypes
 def get_params_out_of_form(
     request: HttpRequest, experiment: Experiment
 ) -> Tuple[bool, Dict[str, List[str]] | Dict[str, Dict[str, HyperparameterTypes]]]:
+    """
+    Extracts the hyperparameters specified in the ExecutionCreateForm.
+    @param request: The HttpRequest containing the fields of the hyperparameters.
+    @param experiment: The experiment of the created execution.
+    @return: A tuple containing a success boolean and a result dictionary.
+    If the boolean is True, the extraction was successful and the result dictionary
+    contains parameter name to parameter value mappings for each algorithm in the
+    experiment.
+    If the boolean is False, the extraction was not successful and the dict contains
+    a mapping from the hyperparameter form_field which caused an error to an error
+    message.
+    """
 
     dikt: Dict[str, Dict[str, HyperparameterTypes]] = dict()
     errors: Dict[str, List[str]] = dict()
