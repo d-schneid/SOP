398880cf129b7d7390fb44b08b0bfd871f26de6e
Jonas-Bergmann <bergmann.jonas31@gmail.com>
2022-08-03 13:01:34 +0200

    Add pydocs to experiments.templatetags.execution_tags

--- webserver/src/main/sop/experiments/templatetags/execution_tags.py
+++ webserver/src/main/sop/experiments/templatetags/execution_tags.py
@@ -10,6 +10,14 @@ register = template.Library()
 def get_parameter_value(
     execution: Execution, algorithm: Algorithm, param_name: str
 ) -> HyperparameterTypes:
+    """
+    A template tag to get the value of a parameter for an algorithm that was specified
+    in an execution. It will wrap string in extra quotes.
+    @param execution: The execution containing the hyperparameter wanted.
+    @param algorithm: The algorithm used in that execution.
+    @param param_name: The name of the parameter of which the specified value is wanted.
+    @return: The value of the parameter specified in the execution.
+    """
     value = execution.algorithm_parameters[str(algorithm.pk)][param_name]
     if isinstance(value, str):
         value = f'"{value}"'
@@ -20,6 +28,13 @@ def get_parameter_value(
 def default_value_of_param(
     algorithm: Algorithm, param_name: str
 ) -> HyperparameterTypes:
+    """
+    A template tag to get the default value of a parameter of an algorithm. It will wrap
+    strings in extra quotes.
+    @param algorithm: The algorithm containing the parameter.
+    @param param_name: The name of the parameter.
+    @return: The value of the parameter.
+    """
     value = algorithm.get_signature_as_dict()[param_name]
     if isinstance(value, str):
         value = f'"{value}"'
