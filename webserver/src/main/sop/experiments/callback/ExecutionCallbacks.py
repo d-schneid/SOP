27c29af231bf0c5655a62e877c5858e487014df5
Jonas-Bergmann <bergmann.jonas31@gmail.com>
2022-08-03 13:39:14 +0200

    Add pydocs to experiments.callbacks.ExecutionCallbacks

--- webserver/src/main/sop/experiments/callback/ExecutionCallbacks.py
+++ webserver/src/main/sop/experiments/callback/ExecutionCallbacks.py
@@ -9,6 +9,16 @@ from experiments.models.execution import get_zip_result_path
 def execution_callback(
     task_id: int, task_state: TaskState.TaskState, progress: float
 ) -> None:
+    """
+    The progress callback function used by the backend execution task. It will update
+    the progress and state of an execution model based on the progress and state of the
+    backend task.
+    @param task_id: the task id of the backend task which will be the executions primary
+    key
+    @param task_state: The TaskState of the backend execution.
+    @param progress: The progress of the backend execution.
+    @return: None
+    """
     print("execution callback", task_id, task_state, progress)
     execution: Execution = Execution.objects.get(pk=task_id)
 
@@ -24,4 +34,10 @@ def execution_callback(
 
 
 def metric_callback(execution: BackendExecution) -> None:
+    """
+    The metric callback used by the backend execution task. It calls metrics in the
+    backend and saves their results in the executions results path before it is zipped.
+    @param execution: The backend execution object on which the metrics will be called.
+    @return: None
+    """
     print("METRIC CALLBACK!!")
