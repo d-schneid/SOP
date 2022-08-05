81e2bf24498682a6a2e40662a8ea34861b2dc4b4
Jonas-Bergmann <bergmann.jonas31@gmail.com>
2022-08-05 16:10:31 +0200

    Generate metrics for a finished execution to the execution result

--- webserver/src/main/sop/experiments/callback/ExecutionCallbacks.py
+++ webserver/src/main/sop/experiments/callback/ExecutionCallbacks.py
@@ -1,14 +1,31 @@
 import os
+from pathlib import Path
 
+from backend.metric.MetricDataPointsAreOutliers import MetricDataPointsAreOutliers
+from backend.metric.MetricSubspaceOutlierAmount import MetricSubspaceOutlierAmount
 from backend.task import TaskState
 from backend.task.execution.core.Execution import Execution as BackendExecution
 from experiments.models import Execution
-from experiments.models.execution import get_zip_result_path, ExecutionStatus
+from experiments.models.execution import (
+    get_zip_result_path,
+    ExecutionStatus,
+    get_result_path,
+)
 
 
 def execution_callback(
     task_id: int, task_state: TaskState.TaskState, progress: float
 ) -> None:
+    """
+    The progress callback function used by the backend execution task. It will update
+    the progress and state of an execution model based on the progress and state of the
+    backend task.
+    @param task_id: the task id of the backend task which will be the executions primary
+    key
+    @param task_state: The TaskState of the backend execution.
+    @param progress: The progress of the backend execution.
+    @return: None
+    """
     print("execution callback", task_id, task_state, progress)
     execution: Execution = Execution.objects.get(pk=task_id)
 
@@ -31,5 +48,36 @@ def execution_callback(
     execution.save()
 
 
-def metric_callback(execution: BackendExecution) -> None:
-    print("METRIC CALLBACK!!")
+def generate_datapoints_metric(metric_dir: Path, be: BackendExecution):
+    assert os.path.isdir(metric_dir)
+    metric_result_path = metric_dir / "datapoints_metric.csv"
+    metric = MetricDataPointsAreOutliers(indices_mapping=be.dataset_indices)
+    metric.compute_metric(
+        metric_result_path=str(metric_result_path),
+        algorithm_directory_paths=be.algorithm_directory_paths,
+    )
+
+
+def generate_subspace_outlier_metric(metric_dir: Path, be: BackendExecution):
+    assert os.path.exists(metric_dir)
+    metric_result_path = metric_dir / "subspace_outliers.csv"
+    metric = MetricSubspaceOutlierAmount()
+    metric.compute_metric(
+        metric_result_path=str(metric_result_path),
+        algorithm_directory_paths=be.algorithm_directory_paths,
+    )
+
+
+def metric_callback(be: BackendExecution) -> None:
+    print(f"metric callback for execution {be.task_id}")
+    execution_pk = be.task_id
+    if not Execution.objects.filter(pk=execution_pk).exists():
+        return
+
+    execution = Execution.objects.get(pk=execution_pk)
+    metric_dir = Path(get_result_path(execution)) / "metrics"
+    assert os.path.isdir(metric_dir.parent)
+    os.makedirs(metric_dir)
+
+    # generate_datapoints_metric(metric_dir, be)
+    generate_subspace_outlier_metric(metric_dir, be)
