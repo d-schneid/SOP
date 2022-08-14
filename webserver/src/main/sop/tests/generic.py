c59abd6b34a64b5387a0aa1e021e891993157adf
Jonas-Bergmann <bergmann.jonas31@gmail.com>
2022-08-14 17:24:57 +0200

    Fix DebugSchedulerMixin to reset the default_scheduler attribute when finished

--- webserver/src/main/sop/tests/generic.py
+++ webserver/src/main/sop/tests/generic.py
@@ -41,6 +41,7 @@ class DebugSchedulerMixin(MixinBase):
         super().tearDownClass()
         if Scheduler._instance is not None:
             Scheduler.get_instance().hard_shutdown()
+            Scheduler.default_scheduler = None
             Scheduler._instance = None
 
 
