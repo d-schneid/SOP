cf4d9061a3ab96b325342f414776d4c018a3b6ce
Finn Ritter <Finn.Ritter@student.kit.edu>
2022-08-14 02:56:23 +0200

    Add requirements for testing with selenium; also install the automatically

--- setup_selenium_testing.sh
+++ setup_selenium_testing.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Script should be run from project root (i.e. git root)
+echo "This script has to be run from the project root (i.e. git root)."
 
 # add the required users for Selenium Test cases to the database
 python3 webserver/src/main/sop/manage.py createuser --username SeleniumTestUser --password this_is_a_test
@@ -7,3 +9,6 @@ python3 webserver/src/main/sop/manage.py createuser --username SeleniumTestAdmin
 # add pyod algorithms to db and install the required algorithms
 pip install -r requirements_pyod_algorithms.txt
 python3 webserver/src/main/sop/manage.py pyodtodb
+
+# install libraries required for testing with Selenium
+pip install -r requirements_test.txt
