6f6aafc436e8087db43330cfbf1b0c873515c447
Jonas-Bergmann <bergmann.jonas31@gmail.com>
2022-08-14 19:25:37 +0200

    Remove unused checks in execution creation view, since these checks will be done by the input widgets of the template

--- webserver/src/main/sop/experiments/views/execution.py
+++ webserver/src/main/sop/experiments/views/execution.py
@@ -146,40 +146,23 @@ class ExecutionCreateView(
 
         # Get subspaces_min out of form and do sanity checks
         subspaces_min = form.cleaned_data["subspaces_min"]
-        if subspaces_min < 0:
-            messages.error(
-                self.request,
-                "Subspaces Min has to be an Integer greater than or equal to 0.",
-            )
-        else:
-            form.instance.subspaces_min = subspaces_min
+        form.instance.subspaces_min = subspaces_min
 
         # Get subspaces_max out of form and do sanity checks
         subspaces_max = form.cleaned_data["subspaces_max"]
-        if subspaces_max < 0:
-            messages.error(
-                self.request,
-                "Subspaces Max has to be an Integer greater than or equal to 0.",
-            )
-            error = True
+
         if subspaces_max > experiment.dataset.dimensions_total:
             messages.error(
                 self.request,
                 f"Subspaces Max has to be smaller than or equal to the dataset dimension count: {experiment.dataset.dimensions_total}.",
             )
             error = True
-        elif 0 <= subspaces_max <= experiment.dataset.dimensions_total:
+        else:
             form.instance.subspaces_max = subspaces_max
 
         # Get subspace_amount out of form and do sanity checks
         subspace_amount = form.cleaned_data["subspace_amount"]
-        if subspace_amount <= 0:
-            messages.error(
-                self.request, "Subspace amount has to be a positive Integer."
-            )
-            error = True
-        else:
-            form.instance.subspace_amount = subspace_amount
+        form.instance.subspace_amount = subspace_amount
 
         # Generate algorithm_parameters out of form inputs
         success, dikt = get_params_out_of_form(self.request, experiment)
@@ -191,15 +174,10 @@ class ExecutionCreateView(
 
         # Get subspace_generation_seed out of form and do sanity checks
         seed: Optional[int] = form.cleaned_data.get("subspace_generation_seed")
-        # If the seed was not specified, it will be set to a random seed during model creation
+        # If the seed was not specified, it will be set to a random seed during
+        # model creation
         if seed:
-            if seed < 0:
-                messages.error(
-                    self.request, f"Seed has to be greater than 0. (is: {seed})."
-                )
-                error = True
-            else:
-                form.instance.subspace_generation_seed = seed
+            form.instance.subspace_generation_seed = seed
 
         # Sanity check that subspaces_min must be smaller than subspaces_max
         if subspaces_min > subspaces_max:
