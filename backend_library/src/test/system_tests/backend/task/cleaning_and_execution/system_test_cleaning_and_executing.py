41ede6d8a312a55222655a8fd107317d8b9262c3
Merlin Opp <ufjqw@student.kit.edu>
2022-08-26 18:15:32 +0200

    Fix test_cleaning_and_execution in SystemTest_CleaningAndExecuting

--- backend_library/src/test/system_tests/backend/task/cleaning_and_execution/system_test_cleaning_and_executing.py
+++ backend_library/src/test/system_tests/backend/task/cleaning_and_execution/system_test_cleaning_and_executing.py
@@ -1,14 +1,17 @@
 import os
 import unittest
 
-from backend.task.TaskHelper import TaskHelper
+from backend.scheduler.DebugScheduler import DebugScheduler
+from backend.scheduler.Scheduler import Scheduler
+from backend.scheduler.UserRoundRobinScheduler import UserRoundRobinScheduler
+from backend.task.TaskState import TaskState
 from backend.task.cleaning.DatasetCleaning import DatasetCleaning
 from backend.task.execution.ParameterizedAlgorithm import ParameterizedAlgorithm
 from backend.task.execution.subspace.RandomizedSubspaceGeneration import \
     RandomizedSubspaceGeneration as rsg
 from backend.task.execution.subspace.UniformSubspaceDistribution import \
     UniformSubspaceDistribution as usd
-from experiments.models import Execution
+from backend.task.execution.core.Execution import Execution
 
 
 class SystemTest_CleaningAndExecuting(unittest.TestCase):
@@ -20,53 +23,24 @@ class SystemTest_CleaningAndExecuting(unittest.TestCase):
 
     _user_id: int = 214
     _task_id: int = 1553
-    # Cleaning setup ###################################################################
-
-    _uncleaned_dataset_path: str = "./test/datasets/canada_climate_uncleaned.csv"
-
-    # Execution setup ##################################################################
-
-    _result_path: str = "./test/system_tests/backend/task/" \
-                        "execution/basic_tests/execution_folder_system_test1"
-    _zipped_result_path: str = _result_path + ".zip"
-    _details_path: str = os.path.join(_result_path, 'details.json')
-
-    # subspace generation
-    _subspace_size_min: int = 1
-    _subspace_size_max: int = 5
-    _subspace_amount = 4
-    _subspace_seed = 42
-    _data_dimensions_count: int = 26
-    _subspace_generation: rsg = rsg(usd(_subspace_size_min, _subspace_size_max),
-                                    _data_dimensions_count, _subspace_amount,
-                                    _subspace_seed)
-
-    # parameterized algorithms
-    _algorithm_result: int = 42
-    _hyper_parameter: dict = {'algorithm_result': _algorithm_result}
-    _display_names: list[str] = ["display_name", "display_name",
-                                 "different_display_name", "display_name"]
-    _directory_names_in_execution: list[str] = ["display_name", "display_name (1)",
-                                                "different_display_name",
-                                                "display_name (2)"]
-
-    _path: str = "./test/algorithms/DebugAlgorithm.py"
-    _root_dir: str = "./test/"
-    _algorithms: list[ParameterizedAlgorithm] = \
-        list([ParameterizedAlgorithm(_path, _hyper_parameter, _display_names[0]),
-              ParameterizedAlgorithm(_path, _hyper_parameter, _display_names[1]),
-              ParameterizedAlgorithm(_path, _hyper_parameter, _display_names[2]),
-              ParameterizedAlgorithm(_path, _hyper_parameter, _display_names[3])])
-
-    _running_path = _result_path + ".I_am_running"
-    _final_zip_path = _result_path + ".zip"
 
     def setUp(self) -> None:
+        # Scheduler
+        Scheduler._instance = None
+        self._rr_scheduler: DebugScheduler = DebugScheduler()
+
+        # Setup
+        self.__setup_cleaning()
+        self.__setup_execution()
+
+        # Clean old Files
+        self.__cleanup_old_files()
+
         # create DatasetCleaning
         self._dc: DatasetCleaning = \
             DatasetCleaning(self._user_id, self._task_id,
                             self.__cleaning_task__progress_callback,
-                            "no_uncleaned_dataset",
+                            self._uncleaned_dataset_path,
                             self._cleaned_dataset_path, None)
 
         # create Execution
@@ -80,17 +54,91 @@ class SystemTest_CleaningAndExecuting(unittest.TestCase):
                                         zip_running_path=self._zipped_result_path)
 
     def test_cleaning_and_execution(self):
-        self.assertEqual(True, False)  # add assertion here
+        # Do the DatasetCleaning # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+        self.assertFalse(os.path.isfile(self._cleaned_dataset_path))
+
+        self._dc.schedule()
+
+        self.assertTrue(os.path.isfile(self._cleaned_dataset_path))
+
+        # Do the Execution # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+        self.assertFalse(os.path.isfile(self._zipped_result_path))
+
+        self._ex.schedule()
+
+        self.assertTrue(os.path.isfile(self._zipped_result_path))
 
     # callbacks ########################################################################
-    def __cleaning_task__progress_callback(self):
+    def __cleaning_task__progress_callback(self, task_id: int,
+                                           task_state: TaskState, progress: float):
         pass
 
-    def __execution_task_progress_callback(self):
+    def __execution_task_progress_callback(self, task_id: int,
+                                           task_state: TaskState, progress: float):
         pass
 
-    def __metric_callback(self):
+    def __metric_callback(self, execution: Execution):
         pass
+
+    # cleanup ##########################################################################
+    def __cleanup_old_files(self):
+        # DatasetCleaning
+        if os.path.isfile(self._cleaned_dataset_path):
+            os.remove(self._cleaned_dataset_path)
+        # Execution
+        if os.path.isfile(self._result_path):
+            os.remove(self._result_path)
+        if os.path.isfile(self._zipped_result_path):
+            os.remove(self._zipped_result_path)
+
+    # setup ############################################################################
+    def __setup_execution(self):
+        # Execution setup ##############################################################
+
+        self._result_path: str = "./test/system_tests/backend/task/" \
+                                 "execution/basic_tests/execution_folder_system_test1"
+        self._zipped_result_path: str = self._result_path + ".zip"
+        self._details_path: str = os.path.join(self._result_path, 'details.json')
+
+        # subspace generation
+        self._subspace_size_min: int = 1
+        self._subspace_size_max: int = 5
+        self._subspace_amount = 4
+        self._subspace_seed = 42
+        self._data_dimensions_count: int = 26
+        self._subspace_generation: rsg = rsg(usd(self._subspace_size_min,
+                                                 self._subspace_size_max),
+                                             self._data_dimensions_count,
+                                             self._subspace_amount,
+                                             self._subspace_seed)
+
+        # parameterized algorithms
+        self._algorithm_result: int = 42
+        hyper_parameter: dict = {'algorithm_result': self._algorithm_result}
+        knn_hyper_parameter: dict = \
+            {'contamination': 0.1,
+             'n_neighbors': 5, 'method': 'largest',
+             'radius': 1.0, 'algorithm': 'auto',
+             'leaf_size': 30,
+             'metric': 'minkowski', 'p': 2,
+             'metric_params': None, 'n_jobs': 1}
+
+        debug_algorithm_path: str = "./test/algorithms/DebugAlgorithm.py"
+        knn_algorithm_path: str = "./test/algorithms/DebugAlgorithm.py"
+
+        self._algorithms: list[ParameterizedAlgorithm] = \
+            list([ParameterizedAlgorithm(debug_algorithm_path, hyper_parameter,
+                                         "debug_algorithm"),
+                  ParameterizedAlgorithm(knn_algorithm_path, knn_hyper_parameter,
+                                         "KNN")])
+
+        self._running_path = self._result_path + ".I_am_running"
+        self._final_zip_path = self._result_path + ".zip"
+
+    def __setup_cleaning(self):
+        # DatasetCleaning setup ########################################################
+        self._uncleaned_dataset_path: str = \
+            "./test/datasets/canada_climate_uncleaned.csv"
 
 
 if __name__ == '__main__':
