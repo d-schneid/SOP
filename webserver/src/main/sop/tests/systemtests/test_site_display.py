b401c3e0e9e749589233073ca9d250ece456a4f9
Finn Ritter <Finn.Ritter@student.kit.edu>
2022-08-30 21:51:55 +0200

    Another attempt to run the Selenium tests in the CI

--- webserver/src/main/sop/tests/systemtests/test_site_display.py
+++ webserver/src/main/sop/tests/systemtests/test_site_display.py
@@ -10,8 +10,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
 
-@skip
+
 class TestSiteDisplay(StaticLiveServerTestCase):
+    @skip
     def test_driver_manager_chrome(self):
         chrome_options = ChromeOptions()
         chrome_options.headless = True
@@ -31,6 +32,7 @@ class TestSiteDisplay(StaticLiveServerTestCase):
 
         print("Chrome successful!")
 
+    @skip
     def test_driver_manager_firefox(self):
         firefox_options = FirefoxOptions()
         firefox_options.add_argument("--headless")
@@ -47,3 +49,47 @@ class TestSiteDisplay(StaticLiveServerTestCase):
         driver.quit()
 
         print("Firefox successful!")
+
+    def test_ci_chrome(self):
+        chrome_options = ChromeOptions()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--window-size=2560,1440")
+        chrome_options.add_argument("--start-maximized")
+
+        chrome_driver = webdriver.Remote(
+            command_executor="http://chrome:4444/wd/hub",
+            options=chrome_options,
+        )
+
+        chrome_driver.get(self.live_server_url)
+
+        print("Test CI Chrome worked so far!\n----------")
+        print("Page source: " + chrome_driver.page_source)
+        print("----------------")
+
+        self.assertEqual(chrome_driver.title, "Login")
+        self.assertIn("Login", chrome_driver.page_source)
+
+        print("TEST CI CHROME WORKED !!!")
+
+    def test_ci_firefox(self):
+        firefox_options = FirefoxOptions()
+        firefox_options.add_argument("--headless")
+        firefox_options.add_argument("--window-size=2560,1440")
+        firefox_options.add_argument("--start-maximized")
+
+        firefox_driver = webdriver.Remote(
+            command_executor="http://firefox:4444/wd/hub",
+            options=firefox_options,
+        )
+
+        firefox_driver.get(self.live_server_url)
+
+        print("Test CI Firefox worked so far!\n----------")
+        print("Page source: " + firefox_driver.page_source)
+        print("----------------")
+
+        self.assertEqual(firefox_driver.title, "Login")
+        self.assertIn("Login", firefox_driver.page_source)
+
+        print("TEST CI FIREFOX WORKED !!!")
