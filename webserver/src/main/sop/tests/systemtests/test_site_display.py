426504d5a73c8346e106c83a22c7bf6bc8d10973
Finn Ritter <Finn.Ritter@student.kit.edu>
2022-08-31 15:03:49 +0200

    Another debug attempt: onyl necessary tests and more debug output

--- webserver/src/main/sop/tests/systemtests/test_site_display.py
+++ webserver/src/main/sop/tests/systemtests/test_site_display.py
@@ -15,39 +15,8 @@ from webdriver_manager.firefox import GeckoDriverManager
 class TestSiteDisplay(StaticLiveServerTestCase):
 
     def test_driver_manager_chrome(self):
-        chrome_options = ChromeOptions()
-        chrome_options.headless = True
 
-        service = ChromeService(executable_path=ChromeDriverManager().install())
-
-        driver = webdriver.Chrome(service=service, options=chrome_options)
-
-        # ATTENTION: Webserver has to run locally
-
-        driver.get(self.live_server_url)
-
-        self.assertEqual(driver.title, "Login")
-        self.assertIn("Login", driver.page_source)
-
-        driver.quit()
-
-        print("Chrome successful!")
-
-
-    def test_driver_manager_firefox(self):
-        firefox_options = FirefoxOptions()
-        firefox_options.add_argument("--headless")
-
-        service = FirefoxService(executable_path=GeckoDriverManager().install())
-
-        driver = webdriver.Firefox(service=service, options=firefox_options)
-
-        driver.get(self.live_server_url)
-
-        self.assertEqual(driver.title, "Login")
-        self.assertIn("Login", driver.page_source)
-
-        print("=============START========")
+        print("======== CHECKING URL accessible in CHROME driver tests =========")
 
         page = urllib.request.urlopen(self.live_server_url)
         print("Status: " + str(page.status))
@@ -55,30 +24,69 @@ class TestSiteDisplay(StaticLiveServerTestCase):
         print("Headers: ------------\n" + str(page.headers) + "\n------------")
         print("Content: -----------\n" + page.read(300).decode("utf-8") + "\n---------")
 
-        print("=======================")
+        print("======= URL check endent ==========")
 
-        page = urllib.request.urlopen("http://localhost:48289/")
+
+        chrome_options = ChromeOptions()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--window-size=2560,1440")
+        chrome_options.add_argument("--start-maximized")
+
+        service = ChromeService(executable_path=ChromeDriverManager().install())
+
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+
+        print("====== START Chrome driver tests ======")
+
+        driver.get(self.live_server_url)
+
+        self.assertEqual(driver.title, "Login")
+        self.assertIn("Login", driver.page_source)
+
+        driver.quit()
+
+        print("======= Chrome successful! =========")
+
+
+    def test_driver_manager_firefox(self):
+
+        print("======== CHECKING URL accessible in FIREFOX driver tests =========")
+
+        page = urllib.request.urlopen(self.live_server_url)
         print("Status: " + str(page.status))
         print("Url: " + str(page.url))
         print("Headers: ------------\n" + str(page.headers) + "\n------------")
         print("Content: -----------\n" + page.read(300).decode("utf-8") + "\n---------")
 
-        print("=========================")
+        print("======= URL check endent ==========")
 
-        driver.get("http://localhost:48289/")
+
+        firefox_options = FirefoxOptions()
+        firefox_options.add_argument("--headless")
+        firefox_options.add_argument("--window-size=2560,1440")
+        firefox_options.add_argument("--start-maximized")
+
+        service = FirefoxService(executable_path=GeckoDriverManager().install())
+
+        driver = webdriver.Firefox(service=service, options=firefox_options)
+
+        print("====== START Firefox driver tests ======")
+
+        driver.get(self.live_server_url)
+
+        self.assertEqual(driver.title, "Login")
+        self.assertIn("Login", driver.page_source)
 
         driver.quit()
 
-        print("Firefox successful!")
+        print("======== Firefox successful! ==========")
 
-
+    @skip
     def test_ci_chrome(self):
         chrome_options = ChromeOptions()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--window-size=2560,1440")
         chrome_options.add_argument("--start-maximized")
-        chrome_options.add_argument("--ignore-ssl-errors=yes")
-        chrome_options.add_argument("--ignore-certificate-errors")
 
         chrome_driver = webdriver.Remote(
             command_executor="http://chrome:4444/wd/hub",
@@ -116,7 +124,7 @@ class TestSiteDisplay(StaticLiveServerTestCase):
 
         print("TEST CI CHROME WORKED !!!")
 
-
+    @skip
     def test_ci_firefox(self):
         firefox_options = FirefoxOptions()
         firefox_options.add_argument("--headless")
