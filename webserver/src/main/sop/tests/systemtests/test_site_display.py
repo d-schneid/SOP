4dca5b34587d5a3f030fca96990ac250fe5cfb00
Finn Ritter <Finn.Ritter@student.kit.edu>
2022-08-31 18:54:24 +0200

    try with less options: Chrome driver

--- webserver/src/main/sop/tests/systemtests/test_site_display.py
+++ webserver/src/main/sop/tests/systemtests/test_site_display.py
@@ -32,7 +32,7 @@ class TestSiteDisplay(StaticLiveServerTestCase):
         chrome_options = ChromeOptions()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
+        #chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument("--window-size=2560,1440")
         chrome_options.add_argument("--start-maximized")
 
