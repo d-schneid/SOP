ab5aa38a9566d1a8db26f93eeb294fd2681a50b4
uhwkd <jonas.bergmann@student.kit.edu>
2022-09-13 14:09:20 +0200

    Revert broken commits

---
+++ webserver/src/main/sop/tests/systemtests/test_dataset_upload.py
@@ -0,0 +1,46 @@
+import os.path
+
+from tests.systemtests.selenium_base_test_cases.SeleniumTestCase import SeleniumTestCase
+
+
+class DatasetUploadTests(SeleniumTestCase):
+    def test_standard_site(self):
+        # login
+        self.login(
+            SeleniumTestCase.STANDARD_USERNAME_USER,
+            SeleniumTestCase.STANDARD_PASSWORD_USER,
+        )
+
+        # for the standard user, admin should not be visible
+        self.assertNotIn("Admin", self.driver.page_source)
+        self.assertNotIn("/admin/login", self.driver.page_source)
+
+    def test_valid_dataset_upload(self):
+        # login
+        self.login(
+            SeleniumTestCase.STANDARD_USERNAME_USER,
+            SeleniumTestCase.STANDARD_PASSWORD_USER,
+        )
+
+        valid_dataset_path = os.path.join(
+            "tests", "sample_datasets", "canada_testing.csv"
+        )
+
+        dataset_name = "Test Dataset: Canada"
+        dataset_description = (
+            "This is the Canada Dataset, used for automated tests with Selenium."
+        )
+
+        self.upload_dataset(
+            dataset_path=valid_dataset_path,
+            dataset_name=dataset_name,
+            dataset_description=dataset_description,
+            username=SeleniumTestCase.STANDARD_USERNAME_USER,
+        )
+
+        self.wait_until_dataset_ready(
+            dataset_name=dataset_name,
+            failure_expected=False,
+        )
+
+        # TODO: download cleaned & check
